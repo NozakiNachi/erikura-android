645662dc119931bef419d190da38534eeadd7610
takayama <takayama@cuebs.co.jp>
2019-12-23 18:04:58 +0900

    スプラッシュ画面の実装

---
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/StartActivity.kt
@@ -0,0 +1,14 @@
+package jp.co.recruit.erikura.presenters.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.co.recruit.erikura.R
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
