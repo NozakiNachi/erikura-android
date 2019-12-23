977232a7dfbfcfcf04439567e806bab0af38d9f0
Takeshi KATO <kato@cuebs.co.jp>
2019-12-23 16:10:24 +0900

    プロジェクトの雛形を追加

---
+++ app/src/androidTest/java/jp/co/recruit/erikura/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package jp.co.recruit.erikura
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("jp.co.recruit.erikura", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/jp/co/recruit/erikura/EmptyActivity.kt
@@ -0,0 +1,12 @@
+package jp.co.recruit.erikura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmptyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empty)
+    }
+}
