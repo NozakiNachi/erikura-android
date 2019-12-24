119939453b09d655f2645c7c62f6e0b3d37b6eb0
Takeshi KATO <kato@cuebs.co.jp>
2019-12-24 21:06:53 +0900

    ログイン画面の実装

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/LoginActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/LoginActivity.kt
@@ -1,14 +1,54 @@
 package jp.co.recruit.erikura.presenters.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import jp.co.recruit.erikura.R
+import jp.co.recruit.erikura.data.network.Api
+import jp.co.recruit.erikura.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginEventHandlers {
+    private val viewModel: LoginViewModel by lazy {
+        ViewModelProvider(this).get(LoginViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.viewModel = viewModel
+        binding.handlers = this
     }
+
+    override fun onClickLogin(view: View) {
+        // FIXME: spinner 表示を行う
+        // FIXME: email, password が空の場合の対応
+        // FIXME: 完了ボタンのカスタマイズ
+        Log.v("EMAIL", viewModel.email.value ?: "")
+        Log.v("PASS", viewModel.password.value ?: "")
+        Api(this).login(viewModel.email.value ?: "", viewModel.password.value ?: "") {
+            Log.v("DEBUG", "ログイン成功: userId=${it.userId}")
+            // FIXME:　地図画面、チュートリアルへの遷移
+            // FIXME:　戻るボタンでの戻り先からは外したい
+        }
+    }
+}
+
+class LoginViewModel: ViewModel() {
+    val email: MutableLiveData<String> = MutableLiveData()
+    val password: MutableLiveData<String> = MutableLiveData()
+}
+
+interface LoginEventHandlers {
+    fun onClickLogin(view: View)
 }
