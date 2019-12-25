3a40b6da0f25b3ad5252efb8ddd128f758fbc8f0
takayama <takayama@cuebs.co.jp>
2019-12-25 18:41:19 +0900

    仮登録画面の実装

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterEmailActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterEmailActivity.kt
@@ -57,6 +57,7 @@ class RegisterEmailActivity : AppCompatActivity(), SendEmailEventHandlers, TextW
     }
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        validate()
     }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -64,12 +65,14 @@ class RegisterEmailActivity : AppCompatActivity(), SendEmailEventHandlers, TextW
     }
 
     override fun afterTextChanged(s: Editable?) {
+        validate()
     }
 
     private fun validate(){
         var valid = true
         if (valid && TextUtils.isEmpty(viewModel.email.value)) {
             valid = false
+            errorText.isInvisible = true
         }else if(valid && !(android.util.Patterns.EMAIL_ADDRESS.matcher(viewModel.email.value ?:"").matches())) {
             valid = false
             errorText.isInvisible = false
