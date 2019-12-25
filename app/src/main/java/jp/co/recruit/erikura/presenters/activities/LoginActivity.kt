6190557ad188208c289a39722b47d8ffe65670c3
Takeshi KATO <kato@cuebs.co.jp>
2019-12-25 20:54:19 +0900

    リマインダリンクなどの実装

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/LoginActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/LoginActivity.kt
@@ -1,13 +1,19 @@
 package jp.co.recruit.erikura.presenters.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.Bindable
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import jp.co.recruit.erikura.BuildConfig
 import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.data.network.Api
 import jp.co.recruit.erikura.databinding.ActivityLoginBinding
@@ -23,6 +29,7 @@ class LoginActivity : AppCompatActivity(), LoginEventHandlers {
         setContentView(R.layout.activity_login)
 
         val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.handlers = this
     }
@@ -39,13 +46,42 @@ class LoginActivity : AppCompatActivity(), LoginEventHandlers {
             // FIXME:　戻るボタンでの戻り先からは外したい
         }
     }
+
+    override fun onClickReminderLink(view: View) {
+        val reminderURLString = BuildConfig.SERVER_BASE_URL + "users/password/new"
+        val intent = Intent(this, WebViewActivity::class.java).apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse(reminderURLString)
+        }
+        startActivity(intent)
+    }
+
+    override fun onClickUnreachLink(view: View) {
+        val unreachURLString = BuildConfig.SERVER_BASE_URL + "users/confirmation/new"
+        val intent = Intent(this, WebViewActivity::class.java).apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse(unreachURLString)
+        }
+        startActivity(intent)
+    }
 }
 
 class LoginViewModel: ViewModel() {
     val email: MutableLiveData<String> = MutableLiveData()
     val password: MutableLiveData<String> = MutableLiveData()
+
+    val isLoginButtonEnabled = MediatorLiveData<Boolean>().also { result ->
+        result.addSource(email) { result.value = isValid() }
+        result.addSource(password) { result.value = isValid()  }
+    }
+
+    fun isValid(): Boolean {
+        return (email.value?.isNotBlank() ?: false) && (password.value?.isNotBlank() ?: false)
+    }
 }
 
 interface LoginEventHandlers {
     fun onClickLogin(view: View)
+    fun onClickReminderLink(view: View)
+    fun onClickUnreachLink(view: View)
 }
---
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/WebViewActivity.kt
@@ -0,0 +1,88 @@
+package jp.co.recruit.erikura.presenters.activities
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import jp.co.recruit.erikura.R
+import java.util.concurrent.ScheduledExecutorService
+
+
+class WebViewActivity : AppCompatActivity() {
+    private val webView: WebView get() = findViewById(R.id.webview_webview)
+    private val progressBar: ProgressBar get() = findViewById(R.id.webview_progress)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+
+        if (savedInstanceState == null) {
+            // Intent から URL を受け取っている
+            val url = intent.dataString
+            Log.d("WebView: ", "URL=${url}")
+
+            webView.requestFocus()
+            webView.settings.apply {
+                // javascript を有効化
+                javaScriptEnabled = true
+                // ズームを可能にします
+                builtInZoomControls = true
+            }
+            webView.webViewClient = object: WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    progressBar.visibility = View.VISIBLE
+                    super.onPageStarted(view, url, favicon)
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    progressBar.visibility = View.INVISIBLE
+                }
+
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    val url = request?.url
+                    val scheme = url?.scheme
+                    // http, https の場合はデフォルトの動作を実行します
+                    if (scheme == "http" || scheme == "https" || scheme == null) {
+                        return super.shouldOverrideUrlLoading(view, request)
+                    }
+                    else {
+                        // 上記以外の場合は、外部Activityを開きます
+                        Intent(Intent.ACTION_VIEW).apply {
+                            data = request?.url
+                            startActivity(this)
+                        }
+                        return true
+                    }
+                }
+            }
+            // PDFリンクでダウンロードされた場合に外部Activityを表示します
+            webView.setDownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
+                val i = Intent(Intent.ACTION_QUICK_VIEW).apply {
+                    data = Uri.parse(url)
+                }
+                startActivity(i)
+            }
+            webView.loadUrl(url)
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
