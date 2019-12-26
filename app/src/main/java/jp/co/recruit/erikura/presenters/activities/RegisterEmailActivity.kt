f95ac9dfa5063a450a993e00361f4843348a5f43
takayama <takayama@cuebs.co.jp>
2019-12-26 19:39:40 +0900

    仮登録画面の修正

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterEmailActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterEmailActivity.kt
@@ -3,35 +3,24 @@ package jp.co.recruit.erikura.presenters.activities
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextUtils
-import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isInvisible
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import jp.co.recruit.erikura.BuildConfig
+import jp.co.recruit.erikura.ErikuraApplication
 import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.data.network.Api
 import jp.co.recruit.erikura.databinding.ActivityRegisterEmailBinding
 
-class RegisterEmailActivity : AppCompatActivity(), SendEmailEventHandlers, TextWatcher {
+class RegisterEmailActivity : AppCompatActivity(), SendEmailEventHandlers {
     private val viewModel: RegisterEmailViewModel by lazy {
         ViewModelProvider(this).get(RegisterEmailViewModel::class.java)
     }
-
-    lateinit var button: Button
-    lateinit var editText: EditText
-    lateinit var errorText: TextView
-    lateinit var mlp: ViewGroup.MarginLayoutParams
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -39,18 +28,10 @@ class RegisterEmailActivity : AppCompatActivity(), SendEmailEventHandlers, TextW
         setContentView(R.layout.activity_register_email)
 
         val binding: ActivityRegisterEmailBinding = DataBindingUtil.setContentView(this, R.layout.activity_register_email)
+        binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.handlers = this
-
-        button = findViewById(R.id.registerEmail_button)
-        button.isEnabled = false
-
-        editText = findViewById(R.id.registerEmail_editText)
-        editText.addTextChangedListener(this)
-
-        errorText = findViewById(R.id.registerEmail_errorTextView)
-        errorText.isInvisible = true
-        mlp = errorText.layoutParams as ViewGroup.MarginLayoutParams
+        viewModel.errorVisibility.value = 8
     }
 
     override fun onClickSendEmail(view: View) {
@@ -80,48 +61,36 @@ class RegisterEmailActivity : AppCompatActivity(), SendEmailEventHandlers, TextW
         }
         startActivity(intent)
     }
-
-    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-        validate()
-    }
-
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        validate()
-    }
-
-    override fun afterTextChanged(s: Editable?) {
-        validate()
-    }
-
-    private fun validate(){
-        var valid = true
-        // バリデーションの設定
-        if (valid && TextUtils.isEmpty(viewModel.email.value)) {
-            valid = false
-            errorText.isInvisible = true
-            errorText.text = ""
-            errorText.textSize = 0f
-            mlp.topMargin = 0
-        }else if(valid && !(android.util.Patterns.EMAIL_ADDRESS.matcher(viewModel.email.value ?:"").matches())) {
-            valid = false
-            errorText.isInvisible = false
-            errorText.text = getString(R.string.email_format_error)
-            errorText.textSize = 14.0f
-            mlp.topMargin = 10
-        }else {
-            valid = true
-            errorText.isInvisible = true
-            errorText.text = ""
-            errorText.textSize = 0f
-            mlp.topMargin = 0
-        }
-        errorText.layoutParams = mlp
-        button.isEnabled = valid
-    }
 }
 
 class RegisterEmailViewModel: ViewModel() {
     val email: MutableLiveData<String> = MutableLiveData()
+    val errorMsg: MutableLiveData<String> = MutableLiveData()
+    val errorVisibility: MutableLiveData<Int> = MutableLiveData()
+
+    val isRegisterEmailButtonEnabled = MediatorLiveData<Boolean>().also { result ->
+        result.addSource(email) { result.value = isValid() }
+    }
+
+    private fun isValid(): Boolean {
+        var valid = true
+
+        if (valid && email.value?.isBlank() ?: true) {
+            valid = false
+            errorMsg.value = ""
+            errorVisibility.value = 8
+        }else if (valid && !(android.util.Patterns.EMAIL_ADDRESS.matcher(email.value ?:"").matches())) {
+            valid = false
+            errorMsg.value = ErikuraApplication.instance.getString(R.string.email_format_error)
+            errorVisibility.value = 0
+        } else {
+            valid = true
+            errorMsg.value = ""
+            errorVisibility.value = 8
+        }
+
+        return valid
+    }
 }
 
 interface SendEmailEventHandlers {
