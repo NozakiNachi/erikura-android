9153a96d5dafe238e7415eab0f60235ef58ebaae
takayama <takayama@cuebs.co.jp>
2019-12-27 17:08:21 +0900

    パスワード登録画面の追加

---
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterPasswordActivity.kt
@@ -0,0 +1,26 @@
+package jp.co.recruit.erikura.presenters.activities
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import jp.co.recruit.erikura.BuildConfig
+import jp.co.recruit.erikura.ErikuraApplication
+import jp.co.recruit.erikura.R
+import jp.co.recruit.erikura.data.network.Api
+import jp.co.recruit.erikura.databinding.ActivityRegisterEmailBinding
+
+class RegisterPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_password)
+    }
+}
