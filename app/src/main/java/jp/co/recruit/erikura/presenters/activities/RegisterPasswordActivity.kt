04cb7d24e0cd0be679ee52142792fa5117ffec44
takayama <takayama@cuebs.co.jp>
2020-01-06 18:27:49 +0900

    パスワード登録画面の実装

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterPasswordActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterPasswordActivity.kt
@@ -15,12 +15,47 @@ import jp.co.recruit.erikura.BuildConfig
 import jp.co.recruit.erikura.ErikuraApplication
 import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.data.network.Api
-import jp.co.recruit.erikura.databinding.ActivityRegisterEmailBinding
+import jp.co.recruit.erikura.databinding.ActivityRegisterPasswordBinding
 
 class RegisterPasswordActivity : AppCompatActivity() {
+    private val viewModel: RegisterPasswordViewModel by lazy {
+        ViewModelProvider(this).get(RegisterPasswordViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_password)
+        //setContentView(R.layout.activity_register_password)
+
+        val binding: ActivityRegisterPasswordBinding = DataBindingUtil.setContentView(this, R.layout.activity_register_email)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        viewModel.errorVisibility.value = 8
+    }
+}
+
+class RegisterPasswordViewModel: ViewModel() {
+    val password: MutableLiveData<String> = MutableLiveData()
+    val errorMsg: MutableLiveData<String> = MutableLiveData()
+    val errorVisibility: MutableLiveData<Int> = MutableLiveData()
+
+    val isNextButtonEnabled = MediatorLiveData<Boolean>().also { result ->
+        result.addSource(password) {result.value = isValid() }
+    }
+
+    private fun isValid(): Boolean {
+        var valid = true
+
+        if (valid && password.value?.isBlank() ?:true) {
+            valid = false
+            errorMsg.value = ""
+            errorVisibility.value = 8
+        }else {
+            valid = true
+            errorMsg.value = ""
+            errorVisibility.value = 8
+        }
+
+        return valid
     }
 }
