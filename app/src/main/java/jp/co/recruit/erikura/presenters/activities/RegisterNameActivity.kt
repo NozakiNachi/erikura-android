c62e948f7425e44c8049434b28c14858655641d1
takayama <takayama@cuebs.co.jp>
2020-01-07 18:13:39 +0900

    名前登録画面の実装

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterNameActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterNameActivity.kt
@@ -15,14 +15,128 @@ import jp.co.recruit.erikura.ErikuraApplication
 import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.business.models.User
 import jp.co.recruit.erikura.data.network.Api
+import jp.co.recruit.erikura.databinding.ActivityRegisterNameBinding
 import jp.co.recruit.erikura.databinding.ActivityRegisterPasswordBinding
 import java.util.regex.Pattern
 
-class RegisterNameActivity : AppCompatActivity() {
+class RegisterNameActivity : AppCompatActivity(), RegisterNameEventHandlers {
+    private val viewModel: RegisterNameViewModel by lazy {
+        ViewModelProvider(this).get(RegisterNameViewModel::class.java)
+    }
+
+    val user: User = User()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_name)
+        //setContentView(R.layout.activity_register_name)
+
+        // FIXME: 前画面からユーザ情報を受け取る
+
+        val binding: ActivityRegisterNameBinding = DataBindingUtil.setContentView(this, R.layout.activity_register_name)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        binding.handlers = this
+        viewModel.lastNameErrorVisibility.value = 8
+        viewModel.firstNameErrorVisibility.value = 8
     }
+
+    override fun onClickNext(view: View) {
+        Log.v("LastName", viewModel.lastName.value ?: "")
+        user.password = viewModel.lastName.value
+        Log.v("FirstName", viewModel.firstName.value ?: "")
+        user.password = viewModel.firstName.value
+    }
+}
+
+class RegisterNameViewModel: ViewModel() {
+    val lastName: MutableLiveData<String> = MutableLiveData()
+    val lastNameErrorMsg: MutableLiveData<String> = MutableLiveData()
+    val lastNameErrorVisibility: MutableLiveData<Int> = MutableLiveData()
+    val firstName: MutableLiveData<String> = MutableLiveData()
+    val firstNameErrorMsg: MutableLiveData<String> = MutableLiveData()
+    val firstNameErrorVisibility: MutableLiveData<Int> = MutableLiveData()
+
+    val isNextButtonEnabled = MediatorLiveData<Boolean>().also { result ->
+        result.addSource(lastName) { result.value = isValid() }
+        result.addSource(firstName) { result.value = isValid()  }
+    }
+
+    private fun isValid(): Boolean {
+        var valid = true
+
+        if (valid && (lastName.value?.isBlank() ?:true || firstName.value?.isBlank() ?:true)) {
+            valid = false
+            lastNameErrorMsg.value = ""
+            lastNameErrorVisibility.value = 8
+        } else if (valid && !(lastName.value?.length ?: 0 < 30)) {
+            valid = false
+            lastNameErrorMsg.value = ErikuraApplication.instance.getString(R.string.last_name_count_error)
+            lastNameErrorVisibility.value = 0
+        } else {
+            valid = true
+            lastNameErrorMsg.value = ""
+            lastNameErrorVisibility.value = 8
+        }
+
+        if (valid && firstName.value?.isBlank() ?:true) {
+            valid = false
+            firstNameErrorMsg.value = ""
+            firstNameErrorVisibility.value = 8
+        } else if (valid && !(firstName.value?.length ?: 0 < 30)) {
+            valid = false
+            firstNameErrorMsg.value = ErikuraApplication.instance.getString(R.string.first_name_count_error)
+            firstNameErrorVisibility.value = 0
+        } else {
+            valid = true
+            firstNameErrorMsg.value = ""
+            firstNameErrorVisibility.value = 8
+        }
+
+        return valid
+    }
+
+    private fun isValidLastName(): Boolean {
+        var valid = true
+
+        if (valid && lastName.value?.isBlank() ?:true) {
+            valid = false
+            lastNameErrorMsg.value = ""
+            lastNameErrorVisibility.value = 8
+        } else if (valid && !(lastName.value?.length ?: 0 < 30)) {
+            valid = false
+            lastNameErrorMsg.value = ErikuraApplication.instance.getString(R.string.last_name_count_error)
+            lastNameErrorVisibility.value = 0
+        } else {
+            valid = true
+            lastNameErrorMsg.value = ""
+            lastNameErrorVisibility.value = 8
+        }
+
+        return valid
+    }
+
+    private fun isValidFirstName(): Boolean {
+        var valid = true
+
+        if (valid && firstName.value?.isBlank() ?:true) {
+            valid = false
+            firstNameErrorMsg.value = ""
+            firstNameErrorVisibility.value = 8
+        } else if (valid && !(firstName.value?.length ?: 0 < 30)) {
+            valid = false
+            firstNameErrorMsg.value = ErikuraApplication.instance.getString(R.string.first_name_count_error)
+            firstNameErrorVisibility.value = 0
+        } else {
+            valid = true
+            firstNameErrorMsg.value = ""
+            firstNameErrorVisibility.value = 8
+        }
+
+        return valid
+    }
+}
+
+interface RegisterNameEventHandlers {
+    fun onClickNext(view: View)
 }
