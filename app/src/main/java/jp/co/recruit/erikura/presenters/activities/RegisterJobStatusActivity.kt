ef963741a993b588a6f0f2c8323d69e303149773
takayama <takayama@cuebs.co.jp>
2020-01-15 18:37:14 +0900

    職業選択画面の実装

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterJobStatusActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/RegisterJobStatusActivity.kt
@@ -11,8 +11,9 @@ import jp.co.recruit.erikura.ErikuraApplication
 import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.business.models.User
 import jp.co.recruit.erikura.data.network.Api
+import jp.co.recruit.erikura.databinding.ActivityRegisterJobStatusBinding
 
-class RegisterJobStatusActivity : AppCompatActivity() {
+class RegisterJobStatusActivity : AppCompatActivity(), RegisterJobStatusEventHandlers {
 
     var user: User = User()
 
@@ -24,5 +25,65 @@ class RegisterJobStatusActivity : AppCompatActivity() {
         // ユーザ情報を受け取る
         user = intent.getParcelableExtra("user")
 
+        val binding: ActivityRegisterJobStatusBinding = DataBindingUtil.setContentView(this, R.layout.activity_register_job_status)
+        binding.lifecycleOwner = this
+        binding.handlers = this
     }
+
+    override fun onClickUnemployed(view: View) {
+        user.jobStatus = "unemployed"
+        moveToNext()
+    }
+
+    override fun onClickHomemaker(view: View) {
+        user.jobStatus= "homemaker"
+        moveToNext()
+    }
+
+    override fun onClickFreelancer(view: View) {
+        user.jobStatus = "freelancer"
+        moveToNext()
+    }
+
+    override fun onClickStudent(view: View) {
+        user.jobStatus = "student"
+        moveToNext()
+    }
+
+    override fun onClickPartTime(view: View) {
+        user.jobStatus = "part_time"
+        moveToNext()
+    }
+
+    override fun onClickEmployee(view: View) {
+        user.jobStatus = "employee"
+        moveToNext()
+    }
+
+    override fun onClickSelfEmployed(view: View) {
+        user.jobStatus = "self_employed"
+        moveToNext()
+    }
+
+    override fun onClickOtherJob(view: View) {
+        user.jobStatus = "other_job"
+        moveToNext()
+    }
+
+    private fun moveToNext() {
+        val intent: Intent = Intent(this@RegisterJobStatusActivity, RegisterWishWorkActivity::class.java)
+        intent.putExtra("user", user)
+        startActivity(intent)
+    }
+}
+
+interface RegisterJobStatusEventHandlers {
+    fun onClickUnemployed(view: View)
+    fun onClickHomemaker(view: View)
+    fun onClickFreelancer(view: View)
+    fun onClickStudent(view: View)
+    fun onClickPartTime(view: View)
+    fun onClickEmployee(view: View)
+    fun onClickSelfEmployed(view: View)
+    fun onClickOtherJob(view: View)
 }
