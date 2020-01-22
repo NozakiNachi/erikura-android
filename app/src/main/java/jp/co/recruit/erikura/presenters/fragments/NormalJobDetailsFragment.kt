3a31b5f619594a3f968228317d3f1af7269c298f
takayama <takayama@cuebs.co.jp>
2020-01-22 18:38:39 +0900

    案件詳細画面(募集中) 背景画像の実装

---
+++ app/src/main/java/jp/co/recruit/erikura/presenters/fragments/NormalJobDetailsFragment.kt
@@ -0,0 +1,71 @@
+package jp.co.recruit.erikura.presenters.fragments
+
+import android.app.Activity
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import jp.co.recruit.erikura.ErikuraApplication
+import jp.co.recruit.erikura.R
+import jp.co.recruit.erikura.business.models.Job
+import jp.co.recruit.erikura.databinding.FragmentNormalJobDetailsBinding
+import android.graphics.drawable.BitmapDrawable
+
+
+class NormalJobDetailsFragment(private val activity: Activity, val job: Job?) : Fragment() {
+    private val viewModel: NormalJobDetailsFragmentViewModel by lazy {
+        ViewModelProvider(this).get(NormalJobDetailsFragmentViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentNormalJobDetailsBinding.inflate(inflater, container, false)
+        viewModel.setup(activity, job)
+        binding.viewModel = viewModel
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val transaction = childFragmentManager.beginTransaction()
+        val timeLabel = TimeLabelFragment(job)
+        val jobInfoView = JobInfoViewFragment(job)
+        val thumbnailImage = ThumbnailImageFragment(activity, job)
+        transaction.add(R.id.jobDetails_timeLabelFragment, timeLabel, "timeLabel")
+        transaction.add(R.id.jobDetails_jobInfoViewFragment, jobInfoView, "jobInfoView")
+        transaction.add(R.id.jobDetails_thumbnailImageFragment, thumbnailImage, "thumbnailImage")
+        transaction.commit()
+    }
+
+}
+
+class NormalJobDetailsFragmentViewModel: ViewModel() {
+    val bitmapDrawable: MutableLiveData<BitmapDrawable> = MutableLiveData()
+
+    fun setup(activity: Activity, job: Job?) {
+        if (job != null){
+            // ダウンロード
+            job.thumbnailUrl?.let { url ->
+                val assetsManager = ErikuraApplication.assetsManager
+
+                assetsManager.fetchImage(activity, url) { result ->
+                    activity.runOnUiThread {
+                        val bitmapReduced = Bitmap.createScaledBitmap(result, 15, 15, true)
+                        val bitmapDraw = BitmapDrawable(bitmapReduced)
+                        bitmapDraw.alpha = 150
+                        bitmapDrawable.value = bitmapDraw
+                    }
+                }
+            }
+
+        }
+    }
+}
