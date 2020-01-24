27528ced05250930978befc7389ea587849094b8
takayama <takayama@cuebs.co.jp>
2020-01-24 11:33:22 +0900

    案件詳細画面(募集中) 地図アプリで開くの実装

--- app/src/main/java/jp/co/recruit/erikura/presenters/fragments/JobDetailsViewFragment.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/fragments/JobDetailsViewFragment.kt
@@ -1,20 +1,31 @@
 package jp.co.recruit.erikura.presenters.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.DynamicDrawableSpan
+import android.text.style.ImageSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import jp.co.recruit.erikura.ErikuraApplication
+import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.business.models.Job
 import jp.co.recruit.erikura.databinding.FragmentJobDetailsViewBinding
+import retrofit2.http.Url
+import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-class JobDetailsViewFragment(val job: Job?) : Fragment() {
+class JobDetailsViewFragment(val job: Job?) : Fragment(), JobDetailsViewFragmentEventHandlers {
     private val viewModel: JobDetailsViewFragmentViewModel by lazy {
         ViewModelProvider(this).get(JobDetailsViewFragmentViewModel::class.java)
     }
@@ -26,7 +37,14 @@ class JobDetailsViewFragment(val job: Job?) : Fragment() {
         val binding = FragmentJobDetailsViewBinding.inflate(inflater, container, false)
         viewModel.setup(job)
         binding.viewModel = viewModel
+        binding.handlers = this
         return binding.root
+    }
+
+    override fun onClickOpenMap(view: View) {
+        val uri = Uri.parse("https://www.google.com/maps/search/?api=1&query=${job?.latitude?:0},${job?.longitude?:0}")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
     }
 
 }
@@ -38,6 +56,8 @@ class JobDetailsViewFragmentViewModel: ViewModel() {
     val summaryTitles: MutableLiveData<String> = MutableLiveData()
     val workingPlace: MutableLiveData<String> = MutableLiveData()
     val workingBuilding: MutableLiveData<String> = MutableLiveData()
+
+    val openMapButtonText: MutableLiveData<SpannableString> = MutableLiveData()
 
     fun setup(job: Job?){
         if (job != null) {
@@ -58,10 +78,21 @@ class JobDetailsViewFragmentViewModel: ViewModel() {
             workingPlace.value = job.place?.workingPlace
             workingBuilding.value = job.place?.workingBuilding
         }
+
+        val str = SpannableString("　地図アプリで開く")
+        val drawable = ContextCompat.getDrawable(ErikuraApplication.instance.applicationContext, R.drawable.link)
+        drawable!!.setBounds(0, 0, 40, 40)
+        val span = ImageSpan(drawable, DynamicDrawableSpan.ALIGN_BASELINE)
+        str.setSpan(span, 0, 1, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        openMapButtonText.value = str
     }
 
     private fun dateToString(date: Date, format: String): String {
         val sdf = SimpleDateFormat(format, Locale.JAPAN)
         return sdf.format(date)
     }
+}
+
+interface JobDetailsViewFragmentEventHandlers {
+    fun onClickOpenMap(view: View)
 }
