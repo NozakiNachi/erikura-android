6ead4373701d96ea7e903eb4f3ce4a6e281f5ee1
takayama <takayama@cuebs.co.jp>
2020-01-28 15:33:13 +0900

    場所詳細画面 サムネイル画像表示の実装

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/job/PlaceDetailActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/job/PlaceDetailActivity.kt
@@ -1,6 +1,9 @@
 package jp.co.recruit.erikura.presenters.activities.job
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.text.Spannable
@@ -45,7 +48,8 @@ class PlaceDetailActivity : AppCompatActivity(), PlaceDetailEventHandlers {
 
     override fun onResume() {
         super.onResume()
-
+        viewModel.thumbnailVisibility.value = 8
+        viewModel.setupThumbnail(this, place)
         val transaction = supportFragmentManager.beginTransaction()
         val workingPlaceView = WorkingPlaceViewFragment(place)
         transaction.replace(R.id.placeDetail_workingPlaceView, workingPlaceView)
@@ -61,6 +65,10 @@ class PlaceDetailActivity : AppCompatActivity(), PlaceDetailEventHandlers {
 
 class PlaceDetailViewModel: ViewModel() {
     val openMapButtonText: MutableLiveData<SpannableString> = MutableLiveData()
+    val bitmap: MutableLiveData<Bitmap> = MutableLiveData()
+    val thumbnailVisibility: MutableLiveData<Int> = MutableLiveData()
+    val bitmapDrawable: MutableLiveData<BitmapDrawable> = MutableLiveData()
+
 
     fun setupMapButton() {
         val str = SpannableString(ErikuraApplication.instance.getString(R.string.openMap))
@@ -69,6 +77,24 @@ class PlaceDetailViewModel: ViewModel() {
         val span = ImageSpan(drawable, DynamicDrawableSpan.ALIGN_BASELINE)
         str.setSpan(span, 0, 1, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
         openMapButtonText.value = str
+    }
+
+    fun setupThumbnail(activity: Activity, place: Place) {
+        // ダウンロード
+        place.thumbnailUrl?.let { url ->
+            val assetsManager = ErikuraApplication.assetsManager
+
+            assetsManager.fetchImage(activity, url) { result ->
+                activity.runOnUiThread {
+                    bitmap.value = result
+                    thumbnailVisibility.value = 0
+                    val bitmapReduced = Bitmap.createScaledBitmap(result, 15, 15, true)
+                    val bitmapDraw = BitmapDrawable(bitmapReduced)
+                    bitmapDraw.alpha = 150
+                    bitmapDrawable.value = bitmapDraw
+                }
+            }
+        }
     }
 
 }
