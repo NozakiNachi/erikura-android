c95597eaca2368fbbf646839f150a7a95b553920
takayama <takayama@cuebs.co.jp>
2020-02-05 11:20:42 +0900

    案件詳細画面(作業前)の追加

--- app/src/main/java/jp/co/recruit/erikura/business/models/Entry.kt
+++ app/src/main/java/jp/co/recruit/erikura/business/models/Entry.kt
@@ -1,30 +1,33 @@
 package jp.co.recruit.erikura.business.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 
+@Parcelize
 data class Entry(
-    var id: Int,
-    var userId: Int,
-    var jobId: Int,
-    var comment: String,
-    var limitAt: Date,
-    var startedAt: Date?,
-    var startedLatitude: Double?,
-    var startedLongitude: Double?,
-    var startedSteps: Int?,
-    var startedDistance: Int?,
-    var startedFloorAsc: Int?,
-    var startedFloorDesc: Int?,
-    var finishedAt: Date?,
-    var finishedLatitude: Double?,
-    var finishedLongitude: Double?,
-    var finishedSteps: Int?,
-    var finishedDistance: Int?,
-    var finishedFloorAsc: Int?,
-    var finishedFloorDesc: Int?,
-    var createdAt: Date,
+    var id: Int = 0,
+    var userId: Int = 0,
+    var jobId: Int = 0,
+    var comment: String? = null,
+    var limitAt: Date? = null,
+    var startedAt: Date? = null,
+    var startedLatitude: Double? = null,
+    var startedLongitude: Double? = null,
+    var startedSteps: Int? = 0,
+    var startedDistance: Int? = 0,
+    var startedFloorAsc: Int? = 0,
+    var startedFloorDesc: Int? = 0,
+    var finishedAt: Date? = null,
+    var finishedLatitude: Double? = null,
+    var finishedLongitude: Double? = null,
+    var finishedSteps: Int? = 0,
+    var finishedDistance: Int? = 0,
+    var finishedFloorAsc: Int? = 0,
+    var finishedFloorDesc: Int? = 0,
+    var createdAt: Date? = null,
     var owner: Boolean = false
-) {
+): Parcelable {
     val isStarted: Boolean get() = (startedAt != null)
     val isFinished: Boolean get() = (finishedAt != null)
 }
