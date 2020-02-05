5d13d35b7e01f447f21e7257aed5d625ad1a9b1c
Takeshi KATO <kato@cuebs.co.jp>
2020-02-05 13:45:04 +0900

    実施済み画面のレイアウトの雛形を追加

---
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/AppliedJobsActivity.kt
@@ -0,0 +1,12 @@
+package jp.co.recruit.erikura.presenters.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.co.recruit.erikura.R
+
+class AppliedJobsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_applied_jobs)
+    }
+}
