844b559c352794b67aef180283a6e5e42b8cc147
takayama <takayama@cuebs.co.jp>
2020-02-06 17:41:30 +0900

    案件詳細画面(作業前) タイムリミット表示の実装

--- app/src/main/java/jp/co/recruit/erikura/presenters/fragments/AppliedJobDetailsFragment.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/fragments/AppliedJobDetailsFragment.kt
@@ -2,8 +2,12 @@ package jp.co.recruit.erikura.presenters.fragments
 
 import android.app.Activity
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,11 +21,15 @@ import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.business.models.Job
 import jp.co.recruit.erikura.business.models.User
 import jp.co.recruit.erikura.databinding.FragmentAppliedJobDetailsBinding
+import java.util.*
+
+
 
 class AppliedJobDetailsFragment(private val activity: AppCompatActivity, val job: Job?, val user: User) : Fragment() {
     private val viewModel: AppliedJobDetailsFragmentViewModel by lazy {
         ViewModelProvider(this).get(AppliedJobDetailsFragmentViewModel::class.java)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,6 +38,7 @@ class AppliedJobDetailsFragment(private val activity: AppCompatActivity, val job
         val binding = FragmentAppliedJobDetailsBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = activity
         viewModel.setup(activity, job, user)
+        updateTimeLimit()
         binding.viewModel = viewModel
         return binding.root
     }
@@ -52,10 +61,38 @@ class AppliedJobDetailsFragment(private val activity: AppCompatActivity, val job
         transaction.add(R.id.appliedJobDetails_mapViewFragment, mapView, "mapView")
         transaction.commit()
     }
+
+    private fun updateTimeLimit() {
+        val str = SpannableStringBuilder()
+        val today = Date().time
+        val limit = job?.entry?.limitAt?.time?: 0
+        val diff: Int = limit.toInt() - today.toInt()
+        if (diff >= 0) {
+            val diffHours = diff / (1000 * 60 * 60)
+            val diffMinutes = (diff % (1000 * 60 * 60)) / (1000 * 60)
+
+            if (diffMinutes == 0) {
+                str.append("あと${diffHours}時間以内\n")
+            }else {
+                str.append("あと${diffHours}時間${diffMinutes}分以内\n")
+            }
+            str.setSpan(ForegroundColorSpan(Color.RED), 0, str.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            str.append(ErikuraApplication.instance.getString(R.string.jobDetails_goWorking))
+            viewModel.timeLimit.value = str
+            viewModel.msgVisibility.value = View.VISIBLE
+        }else {
+            str.append(ErikuraApplication.instance.getString(R.string.jobDetails_overLimit))
+            str.setSpan(R.color.colorAccent, 0, str.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            viewModel.timeLimit.value = str
+            viewModel.msgVisibility.value = View.GONE
+        }
+    }
 }
 
 class AppliedJobDetailsFragmentViewModel: ViewModel() {
     val bitmapDrawable: MutableLiveData<BitmapDrawable> = MutableLiveData()
+    val timeLimit: MutableLiveData<SpannableStringBuilder> = MutableLiveData()
+    val msgVisibility: MutableLiveData<Int> = MutableLiveData(View.VISIBLE)
 
     fun setup(activity: Activity, job: Job?, user: User) {
         if (job != null){
