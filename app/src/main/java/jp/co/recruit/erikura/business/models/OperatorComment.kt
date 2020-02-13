69a04c3f182274250c614c78e2891bb3ac926de1
Takeshi KATO <kato@cuebs.co.jp>
2020-02-13 19:24:08 +0900

    Parcel関連の記述を追加

--- app/src/main/java/jp/co/recruit/erikura/business/models/OperatorComment.kt
+++ app/src/main/java/jp/co/recruit/erikura/business/models/OperatorComment.kt
@@ -1,9 +1,12 @@
 package jp.co.recruit.erikura.business.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 
+@Parcelize
 data class OperatorComment(
-    var id: Int,
-    var body: String,
-    var createdAt: Date
-)
+    var id: Int = 0,
+    var body: String = "",
+    var createdAt: Date = Date()
+) : Parcelable
--- app/src/main/java/jp/co/recruit/erikura/business/models/OutputSummary.kt
+++ app/src/main/java/jp/co/recruit/erikura/business/models/OutputSummary.kt
@@ -1,19 +1,24 @@
 package jp.co.recruit.erikura.business.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
+
+@Parcelize
 data class OutputSummary(
-    var id: Int?,
-    var place: String?,
-    var evaluation: String?,
-    var latitude: Double?,
-    var longitude: Double?,
-    var photoTakedAt: Double?,
-    var comment: String?,
-    var beforeCleaningPhotoToken: String?,
-    var beforeCleaningPhotoUrl: String?,
+    var id: Int? = null,
+    var place: String? = null,
+    var evaluation: String? = null,
+    var latitude: Double? = null,
+    var longitude: Double? = null,
+    var photoTakedAt: Double? = null,
+    var comment: String? = null,
+    var beforeCleaningPhotoToken: String? = null,
+    var beforeCleaningPhotoUrl: String? = null,
     var operatorLikes: Boolean = false,
-    var operatorComments: List<OperatorComment> = listOf(),
+    var operatorComments: List<@RawValue OperatorComment> = listOf(),
     var willDelete: Boolean = false
-) {
+) : Parcelable {
     // photoAsset
     // isUploadCompleted
     // validate
