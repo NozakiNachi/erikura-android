1bfba3efd7af0761c33b32c7e75f3805ba1edf10
Takeshi KATO <kato@cuebs.co.jp>
2020-02-18 12:36:21 +0900

    ログイン必須画面の実装。ログアウト処理で、保存済みのトークン情報のクリアを追加

---
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/errors/LoginRequiredActivity.kt
@@ -0,0 +1,47 @@
+package jp.co.recruit.erikura.presenters.activities.errors
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import jp.co.recruit.erikura.R
+import jp.co.recruit.erikura.databinding.ActivityLoginRequiredBinding
+import jp.co.recruit.erikura.presenters.activities.LoginActivity
+import jp.co.recruit.erikura.presenters.activities.RegisterEmailActivity
+
+class LoginRequiredActivity : AppCompatActivity(), LoginRequiredHandlers {
+    private val viewModel: LoginRequiredViewModel by lazy {
+        ViewModelProvider(this).get(LoginRequiredViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityLoginRequiredBinding = DataBindingUtil.setContentView(this, R.layout.activity_login_required)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        binding.handlers = this
+    }
+
+    override fun onClickRegisterButton(view: View) {
+        val intent = Intent(this, RegisterEmailActivity::class.java)
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+    }
+
+    override fun onClickLoginButton(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+    }
+}
+
+class LoginRequiredViewModel: ViewModel() {
+}
+
+interface LoginRequiredHandlers {
+    fun onClickRegisterButton(view: View)
+    fun onClickLoginButton(view: View)
+}
