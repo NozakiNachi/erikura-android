a35a9543d422f68eadf923fdb8258ae31f07e1f9
takayama <takayama@cuebs.co.jp>
2020-03-02 17:58:30 +0900

    作業報告確認画面 実施箇所の一覧画像表示処理の追加

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/report/ReportConfirmActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/report/ReportConfirmActivity.kt
@@ -5,17 +5,23 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.business.models.Job
 import jp.co.recruit.erikura.business.models.MediaItem
+import jp.co.recruit.erikura.business.models.OutputSummary
 import jp.co.recruit.erikura.databinding.ActivityReportConfirmBinding
+import jp.co.recruit.erikura.databinding.FragmentReportImageItemBinding
 import jp.co.recruit.erikura.presenters.activities.WebViewActivity
 
 
@@ -25,6 +31,7 @@ class ReportConfirmActivity : AppCompatActivity(), ReportConfirmEventHandlers {
     }
     var job = Job()
     private val EDIT_DATA: Int = 1001
+    private lateinit var reportImageAdapter: ReportImageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +43,11 @@ class ReportConfirmActivity : AppCompatActivity(), ReportConfirmEventHandlers {
         binding.handlers = this
 
         job = intent.getParcelableExtra<Job>("job")
+
+        reportImageAdapter = ReportImageAdapter(this, listOf())
+        val reportImageView: RecyclerView = findViewById(R.id.report_confirm_report_images)
+        reportImageView.adapter = reportImageAdapter
+
         loadData()
     }
 
@@ -94,6 +106,7 @@ class ReportConfirmActivity : AppCompatActivity(), ReportConfirmEventHandlers {
 
     private fun loadData() {
         job.report?.let {
+            reportImageAdapter.summaries = it.outputSummaries
             val minute = it.workingMinute?: 0
             viewModel.workingTime.value = if(minute == 0){""}else {"${minute}分"}
             val item = it.additionalPhotoAsset?: MediaItem()
@@ -136,4 +149,46 @@ interface ReportConfirmEventHandlers {
     fun onClickEditWorkingTime(view: View)
     fun onClickEditEvaluation(view: View)
     fun onClickManual(view: View)
+}
+
+// 実施箇所の一覧
+class ReportImageItemViewModel(activity: Activity, view: View, mediaItem: MediaItem?): ViewModel() {
+    private val imageView: ImageView = view.findViewById(R.id.report_image_item)
+    init {
+        mediaItem?.let {
+            mediaItem.loadImage(activity, imageView)
+        }
+    }
+}
+
+class ReportImageViewHolder(val binding: FragmentReportImageItemBinding): RecyclerView.ViewHolder(binding.root)
+
+class ReportImageAdapter(val activity: FragmentActivity, var summaries: List<OutputSummary>): RecyclerView.Adapter<ReportImageViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReportImageViewHolder {
+        val binding = DataBindingUtil.inflate<FragmentReportImageItemBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.fragment_report_image_item,
+            parent,
+            false
+        )
+
+        val view = binding.root
+        val height = (parent.measuredWidth-40) / 3
+        (view.layoutParams as? RecyclerView.LayoutParams)?.let { layoutParams ->
+            layoutParams.height = height
+            view.layoutParams = layoutParams
+        }
+
+        return ReportImageViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return summaries.count()
+    }
+
+    override fun onBindViewHolder(holder: ReportImageViewHolder, position: Int) {
+        val view = holder.binding.root
+        holder.binding.lifecycleOwner = activity
+        holder.binding.viewModel = ReportImageItemViewModel(activity, view, summaries[position].photoAsset)
+    }
 }
