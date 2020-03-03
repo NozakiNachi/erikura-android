cee02383835d99d334b8c7f5ca6eed519d5854c6
TakayamaTokiko <takayama@cuebs.co.jp>
2020-03-03 12:23:14 +0900

    作業報告確認画面 実施箇所の一覧写真追加ボタン処理の追加

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/report/ReportConfirmActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/report/ReportConfirmActivity.kt
@@ -5,9 +5,11 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -32,6 +34,7 @@ class ReportConfirmActivity : AppCompatActivity(), ReportConfirmEventHandlers {
     }
     var job = Job()
     private val EDIT_DATA: Int = 1001
+    private val GET_FILE: Int = 2001
     private lateinit var reportImageAdapter: ReportImageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -106,13 +109,58 @@ class ReportConfirmActivity : AppCompatActivity(), ReportConfirmEventHandlers {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == EDIT_DATA) {
-            if (resultCode == Activity.RESULT_OK) {
+        when(requestCode) {
+            // 編集画面から戻ってきたとき
+            EDIT_DATA -> {
                 data?.let {
                     job = data.getParcelableExtra<Job>("job")
-                    loadData()
                 }
             }
+            // ギャラリーから戻ってきたとき
+            GET_FILE -> {
+                val uri = data?.data
+                uri?.let {
+                    val cursor = this.contentResolver.query(
+                        uri,
+                        arrayOf(
+                            MediaStore.Files.FileColumns._ID,
+                            MediaStore.MediaColumns.DISPLAY_NAME,
+                            MediaStore.MediaColumns.MIME_TYPE,
+                            MediaStore.MediaColumns.SIZE
+                        ),
+                        MediaStore.MediaColumns.SIZE + ">0",
+                        arrayOf<String>(),
+                        "datetaken DESC"
+                    )
+
+                    cursor?.moveToFirst()
+                    cursor?.let {
+                        // val item = MediaItem.from(cursor)
+                        // MEMO: cursorを渡すとIDの値が0になるので手動で値を入れています
+                        val uriString = uri.toString()
+                        val arr = uriString.split("%3A")
+                        val id = arr.last().toLong()
+                        val mimeType = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.MIME_TYPE))
+                        val size = cursor.getLong(cursor.getColumnIndex(MediaStore.MediaColumns.SIZE))
+                        val item = MediaItem(id = id, mimeType = mimeType, size = size, contentUri = uri)
+                        val summary = OutputSummary()
+                        summary.photoAsset = item
+                        var outputSummaryList: MutableList<OutputSummary> = mutableListOf()
+                        outputSummaryList = job.report?.outputSummaries?.toMutableList()?: mutableListOf()
+                        outputSummaryList.add(summary)
+                        job.report?.let {
+                            it.outputSummaries = outputSummaryList
+                        }
+                    }
+
+                    cursor?.close()
+                }
+            }
+
+        }
+
+        if (resultCode == Activity.RESULT_OK) {
+            loadData()
         }
     }
 
@@ -121,14 +169,18 @@ class ReportConfirmActivity : AppCompatActivity(), ReportConfirmEventHandlers {
         intent.action = Intent.ACTION_OPEN_DOCUMENT
         intent.addCategory(Intent.CATEGORY_OPENABLE)
         intent.type = "image/*"
-        startActivityForResult(intent, 2000 )
+        startActivityForResult(intent, GET_FILE )
     }
 
     private fun loadData() {
         job.report?.let {
+            // 実施箇所の更新
             reportImageAdapter.summaries = it.outputSummaries
+            reportImageAdapter.notifyDataSetChanged()
+            // 作業時間の更新
             val minute = it.workingMinute?: 0
             viewModel.workingTime.value = if(minute == 0){""}else {"${minute}分"}
+            // マニュアル外報告の更新
             val item = it.additionalPhotoAsset?: MediaItem()
             if (item.contentUri != null) {
                 val imageView: ImageView = findViewById(R.id.report_confirm_other_image)
@@ -139,6 +191,7 @@ class ReportConfirmActivity : AppCompatActivity(), ReportConfirmEventHandlers {
             }
             val additionalComment = it.additionalComment?: ""
             viewModel.otherFormComment.value = additionalComment
+            // 案件評価の更新
             val evaluation = it.evaluation?: ""
             when(evaluation) {
                 "good" ->
@@ -219,7 +272,8 @@ class ReportImageAdapter(val activity: FragmentActivity, var summaries: List<Out
             holder.binding.viewModel = ReportImageItemViewModel(activity, view, summaries[position].photoAsset)
         }else {
             holder.binding.viewModel = ReportImageItemViewModel(activity, view, null)
-            holder.binding.root.setOnClickListener {
+            val button = holder.binding.root.findViewById<Button>(R.id.report_image_add_photo_button)
+            button.setOnClickListener {
                 onClickListener?.apply {
                     onClick(view)
                 }
@@ -232,3 +286,5 @@ class ReportImageAdapter(val activity: FragmentActivity, var summaries: List<Out
         fun onClick(view: View)
     }
 }
+
+// 実施箇所
