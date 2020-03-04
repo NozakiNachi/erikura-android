ccbbd03ffadd23b9adb359f8a4f0ca9cbf5f5cfd
tanaka <tanaka@cuebs.co.jp>
2020-03-04 12:52:03 +0900

    テスト用コードコメントアウト

--- app/src/main/java/jp/co/recruit/erikura/presenters/fragments/ReportedJobStatusFragment.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/fragments/ReportedJobStatusFragment.kt
@@ -28,73 +28,17 @@ class ReportedJobStatusFragment (private val activity: AppCompatActivity, val re
     ): View? {
         // Inflate the layout for this fragment
         container?.removeAllViews()
+        viewModel.reportStatus = report.status
         val binding = FragmentReportedJobStatusBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = activity
-        viewModel.setup(activity, report, reportStatus)
         binding.viewModel = viewModel
         return binding.root
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-}
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReportedJobStatusFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ReportedJobStatusFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
 
 class ReportedJobStatusFragmentViewModel: ViewModel() {
 
-    var reportStatus: ReportStatus = Report.status
+    var reportStatus: ReportStatus = ReportStatus.Unconfirmed
         set(reportStatus) {
             field = reportStatus
             when (field){
