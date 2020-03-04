bccf0453c4fa8b1cf3c88f58fe03ec22100bbc66
Takeshi KATO <kato@cuebs.co.jp>
2020-03-04 10:18:20 +0900

    フォアグラウンド時の push 通知の表示を修正

--- app/src/main/java/jp/co/recruit/erikura/services/ErikuraMessagingService.kt
+++ app/src/main/java/jp/co/recruit/erikura/services/ErikuraMessagingService.kt
@@ -1,29 +1,27 @@
 package jp.co.recruit.erikura.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import jp.co.recruit.erikura.ErikuraApplication
+import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.Tracking
+import jp.co.recruit.erikura.presenters.activities.StartActivity
 
 class ErikuraMessagingService : FirebaseMessagingService() {
-
-//    override fun onBind(intent: Intent): IBinder {
-//        TODO("Return the communication channel to the service.")
-//    }
-
-    override fun onNewToken(token: String) {
-        super.onNewToken(token)
-        Tracking.refreshFcmToken(token)
-    }
-}
-/*
-class MyFirebaseMessagingService: FirebaseMessagingService() {
-    private val TAG = "MyFirebaseMessagingService"
-
-    // FIXME: 実装を行う
-
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
 
-        Log.v(TAG, "From: ${remoteMessage.from}")
+        /*
         if (remoteMessage.data.isNotEmpty()) {
             Log.v(TAG, "Message data payload: ${remoteMessage.data}")
 
@@ -33,10 +31,38 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         remoteMessage.notification?.let {
             Log.v(TAG, "Message Notification Body: ${it.body}")
         }
+         */
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.post {
+            Toast.makeText(ErikuraApplication.instance, remoteMessage.notification?.body ?: "通知がありました", Toast.LENGTH_LONG).show()
+        }
+
+        // FIXME: data["extra"] で JSON データが取得できそう
+
+        // FIXME: URL をパースする必要があるのか？
+        val intent = Intent(this, StartActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val channelId = "Default"
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(remoteMessage.notification?.title)
+            .setContentText(remoteMessage.notification?.body)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, "Default channel", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, builder.build())
     }
 
-    fun scheduleJob() {
-        // FIXME: メッセージに対する何らかの処理をする？
+
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Tracking.refreshFcmToken(token)
     }
 }
-*/
