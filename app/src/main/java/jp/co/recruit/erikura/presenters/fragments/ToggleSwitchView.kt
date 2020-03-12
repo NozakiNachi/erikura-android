dc8d0b388617935d14db66aa0c8d6fc3053f0041
野崎那知 <nozaki@cuebs.co.jp>
2020-03-12 17:32:22 +0900

    通知設定画面項目追加

--- app/src/main/java/jp/co/recruit/erikura/presenters/fragments/ToggleSwitchView.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/fragments/ToggleSwitchView.kt
@@ -1,147 +1,57 @@
 package jp.co.recruit.erikura.presenters.fragments
 
 import android.content.Context
+import android.graphics.Rect
 import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.view.View
+import android.view.*
+import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
-import androidx.databinding.DataBindingUtil
+import android.widget.NumberPicker
+import android.widget.ToggleButton
+import androidx.databinding.*
+import androidx.recyclerview.widget.RecyclerView
+import jp.co.recruit.erikura.ErikuraApplication.Companion.applicationContext
 import jp.co.recruit.erikura.R
+import jp.co.recruit.erikura.business.models.JobQuery
+import jp.co.recruit.erikura.databinding.FragmentSearchBarConditionItemBinding
+import jp.co.recruit.erikura.databinding.FragmentSearchBarConditionLabelBinding
 import jp.co.recruit.erikura.databinding.FragmentToggleSwitchViewBinding
+import jp.co.recruit.erikura.presenters.activities.job.ItemPicker
+import jp.co.recruit.erikura.presenters.activities.job.PickerItem
 
 class ToggleSwitchView : FrameLayout, ToggleSwitchHandlers {
+
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         val binding: FragmentToggleSwitchViewBinding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.fragment_toggle_switch_view, this, true)
         binding.handlers = this
+
     }
 
     override fun onClick(view: View) {
-        TODO("Not yet implemented")
+        val btnAnim = AnimationUtils.loadAnimation(
+            applicationContext, R.anim.toggle_btn_on
+        )
+        val bgWhiteAnim = AnimationUtils.loadAnimation(
+            applicationContext, R.anim.toggle_bg_white_on
+        )
+        val bgGreenAnim = AnimationUtils.loadAnimation(
+            applicationContext, R.anim.toggle_bg_green_on
+        )
+
+        var mTglBtn = findViewById<View>(R.id.notification) as ToggleButton
+        var mTglBgWhite = findViewById(R.id.white) as View
+        var mTglBgGreen = findViewById(R.id.green) as View
+
+        mTglBtn.startAnimation(btnAnim)
+        mTglBgWhite.startAnimation(bgWhiteAnim)
+        mTglBgGreen.startAnimation(bgGreenAnim)
     }
 }
+
+
 
 interface ToggleSwitchHandlers {
     fun onClick(view: View)
 }
-
-
-
-/*
-class SearchBarView : RelativeLayout {
-    var adapter: SearchBarConditionsAdapter
-    private var conditionsView: RecyclerView
-
-    constructor(context: Context) : this(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        LayoutInflater.from(context).inflate(R.layout.fragment_search_bar, this, true)
-
-        adapter = SearchBarConditionsAdapter(listOf(JobQuery.CURRENT_LOCATION))
-        conditionsView = findViewById(R.id.search_bar_conditions)
-        conditionsView.setHasFixedSize(true)
-        conditionsView.adapter = adapter
-        conditionsView.addItemDecoration(SearchBarConditionItemDecorator())
-
-        val tapGestureDetector = GestureDetector(context, object: GestureDetector.SimpleOnGestureListener() {
-            override fun onDoubleTap(e: MotionEvent?): Boolean {
-                return true
-            }
-
-            override fun onSingleTapUp(e: MotionEvent?): Boolean {
-                this@SearchBarView.callOnClick()
-                return true
-            }
-        })
-        conditionsView.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
-            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
-                return true
-            }
-
-            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
-                tapGestureDetector.onTouchEvent(e)
-            }
-
-            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
-            }
-        })
-    }
-}
-
-object SearchBarViewAdapter {
-    @BindingAdapter("conditions")
-    @JvmStatic
-    fun setConditions(view: SearchBarView, conditions: List<String>?) {
-        view.adapter.conditions = conditions ?: listOf(JobQuery.CURRENT_LOCATION)
-        view.adapter.notifyDataSetChanged()
-    }
-}
-
-class SearchBarConditionsHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
-
-class SearchBarConditionsAdapter(var conditions: List<String>) : RecyclerView.Adapter<SearchBarConditionsHolder>() {
-    companion object {
-        const val VIEW_CONDITION = 1
-        const val VIEW_LABEL = 2
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchBarConditionsHolder {
-        val binding = when(viewType) {
-            VIEW_CONDITION -> DataBindingUtil.inflate<FragmentSearchBarConditionItemBinding>(
-                    LayoutInflater.from(parent.context),
-                    R.layout.fragment_search_bar_condition_item,
-                    parent,
-                    false)
-            VIEW_LABEL -> DataBindingUtil.inflate<FragmentSearchBarConditionLabelBinding>(
-                    LayoutInflater.from(parent.context),
-                    R.layout.fragment_search_bar_condition_label,
-                    parent,
-                    false)
-            else -> DataBindingUtil.inflate<FragmentSearchBarConditionItemBinding>(
-                    LayoutInflater.from(parent.context),
-                    R.layout.fragment_search_bar_condition_item,
-                    parent,
-                    false)
-
-        }
-        return SearchBarConditionsHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: SearchBarConditionsHolder, position: Int) {
-        when(holder.binding) {
-            is FragmentSearchBarConditionLabelBinding -> {
-                // 何も行いません
-            }
-            is FragmentSearchBarConditionItemBinding -> {
-                holder.binding.condition = conditions[position]
-            }
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return conditions.size + 1
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return if (position < conditions.size) {
-            VIEW_CONDITION
-        }
-        else {
-            VIEW_LABEL
-        }
-    }
-}
-
-class SearchBarConditionItemDecorator: RecyclerView.ItemDecoration() {
-    override fun getItemOffsets(
-        outRect: Rect,
-        view: View,
-        parent: RecyclerView,
-        state: RecyclerView.State
-    ) {
-        super.getItemOffsets(outRect, view, parent, state)
-        outRect.left = view.resources.getDimensionPixelSize(R.dimen.condition_margin)
-    }
-}
- */
