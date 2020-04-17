3cc80add461d2d0dc94634a3915b238ab5f65fea
Takeshi KATO <kato@cuebs.co.jp>
2020-04-17 15:18:51 +0900

    不要な import 文の削除

--- app/src/main/java/jp/co/recruit/erikura/presenters/fragments/WorkingJobDetailsFragment.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/fragments/WorkingJobDetailsFragment.kt
@@ -14,7 +14,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toBitmap
 import androidx.core.os.bundleOf
