db0727dab447eead1959810254633d99310ec213
Takeshi KATO <kato@cuebs.co.jp>
2020-05-05 12:38:18 +0900

    ボタンのダブルタップ対策

---
+++ app/src/main/java/jp/co/recruit/erikura/presenters/util/SafeOnClickListener.kt
@@ -0,0 +1,46 @@
+package jp.co.recruit.erikura.presenters.util
+
+import android.os.SystemClock
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+abstract class SafeOnClickListener(
+    private val safeClickInterval: Long = 1000
+) : View.OnClickListener {
+    private var lastClickTime: Long = 0
+
+    override fun onClick(v: View?) {
+        val now = SystemClock.elapsedRealtime()
+        if (now - lastClickTime < safeClickInterval) {
+            // 前回のクリック時間から、一定時間経過していない場合は、イベントを無視します
+            return
+        }
+        lastClickTime = now
+        onSafeClick(v)
+    }
+
+    abstract fun onSafeClick(v: View?)
+}
+
+fun View.setOnSafeClickListener(listener: View.OnClickListener) {
+    setOnClickListener(object: SafeOnClickListener() {
+        override fun onSafeClick(v: View?) {
+            listener.onClick(v)
+        }
+    })
+}
+
+fun View.setOnSafeClickListener(onSafeClickHandler: (View?) -> Unit) {
+    setOnClickListener(object: SafeOnClickListener() {
+        override fun onSafeClick(v: View?) {
+            onSafeClickHandler(v)
+        }
+    })
+}
+
+object SafeOnClickBindingAdapter {
+    @BindingAdapter("onSafeClick")
+    @JvmStatic fun setOnSafeClick(view: View, listener: View.OnClickListener) {
+        view.setOnSafeClickListener(listener)
+    }
+}
