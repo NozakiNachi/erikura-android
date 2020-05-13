64c704483c8ba3d79a8a45412975e6e69ec98bad
Takeshi KATO <kato@cuebs.co.jp>
2020-05-13 17:29:29 +0900

    リストビューのタブ選択処理を修正

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/job/ListViewActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/job/ListViewActivity.kt
@@ -313,7 +313,7 @@ class ListViewActivity : BaseTabbedActivity(R.id.tab_menu_search_jobs), ListView
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // onStart で来た場合には、選択処理はスキップさせます
-        if (onTabButtonInitialization) { return false }
+        if (onTabButtonInitialization) { return true }
 
         // 仕事を探すがタップされている場合は、地図画面に遷移させます
         if (item.itemId == R.id.tab_menu_search_jobs) {
