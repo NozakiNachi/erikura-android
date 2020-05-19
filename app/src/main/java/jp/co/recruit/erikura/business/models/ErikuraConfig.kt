41a7bdd7eb84731f2ce02215b975321e379cdb02
Takeshi KATO <kato@cuebs.co.jp>
2020-05-19 16:50:43 +0900

    お問い合わせ,FAQ,推奨環境のURLをErikuraConfigから取得するように修正

--- app/src/main/java/jp/co/recruit/erikura/business/models/ErikuraConfig.kt
+++ app/src/main/java/jp/co/recruit/erikura/business/models/ErikuraConfig.kt
@@ -7,9 +7,16 @@ import jp.co.recruit.erikura.data.network.ErikuraConfigValue
 object ErikuraConfig {
     const val REWARD_RANGE_KEY = "reward_range"
     const val WORKING_TIME_RANGE_KEY = "working_time_range"
+    const val INQUIRY_URL_KEY = "inquiry_url"
+    const val FAQ_URL_KEY = "faq_url"
+    const val RECOMMENDED_URL_KEY = "recommented_environment_url"
 
     var rewardRange: List<Int> = listOf(1,5,10,50,100,500,1000,1500,2000,2500,3000,3500,4000,4500,5000)
     var workingTimeRange: List<Int> = listOf(1,5,10,15,30,45,60,75,90,105,120,180,240,300,360,420,480)
+    var inquiryURLString: String = "https://support.erikura.net/"
+    var frequentlyQuestionsURLString: String = "https://faq.erikura.net/hc/ja/sections/360003690953-FAQ"
+    var recommendedEnvironmentURLString: String =
+        "https://faq.erikura.net/hc/ja/articles/360020286793-%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AE%E6%8E%A8%E5%A5%A8%E7%92%B0%E5%A2%83%E3%82%92%E6%95%99%E3%81%88%E3%81%A6%E3%81%8F%E3%81%A0%E3%81%95%E3%81%84"
 
     fun load(context: Context) {
         Api(context).erikuraConfig { result ->
@@ -18,6 +25,21 @@ object ErikuraConfig {
             }
             result[WORKING_TIME_RANGE_KEY]?.let { v ->
                 workingTimeRange = (v as ErikuraConfigValue.DoubleList).values.map{ it.toInt() }
+            }
+            result[FAQ_URL_KEY]?.let { v ->
+                (v as? ErikuraConfigValue.StringValue)?.let {
+                    frequentlyQuestionsURLString = it.value ?: frequentlyQuestionsURLString
+                }
+            }
+            result[INQUIRY_URL_KEY]?.let { v ->
+                (v as? ErikuraConfigValue.StringValue)?.let {
+                    inquiryURLString = it.value ?: inquiryURLString
+                }
+            }
+            result[RECOMMENDED_URL_KEY]?.let { v ->
+                (v as? ErikuraConfigValue.StringValue)?.let {
+                    recommendedEnvironmentURLString = it.value ?: recommendedEnvironmentURLString
+                }
             }
         }
     }
