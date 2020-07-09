e6c2c88c7b8749ffc081a60f6a8ef2b681b355cc
RyoyaTanaka <tanaka@cuebs.co.jp>
2020-07-09 19:59:03 +0900

    画像、pdfのリスト表示を実装中(refs=#feature/ERIKURA-1370)

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/job/PropertyNotesActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/job/PropertyNotesActivity.kt
@@ -1,17 +1,19 @@
 package jp.co.recruit.erikura.presenters.activities.job
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
+import android.content.ContentResolver
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Rect
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.MutableLiveData
@@ -25,6 +27,7 @@ import jp.co.recruit.erikura.R
 import jp.co.recruit.erikura.business.models.Caution
 import jp.co.recruit.erikura.business.models.CautionFile
 import jp.co.recruit.erikura.business.models.ErikuraConfig
+import jp.co.recruit.erikura.business.models.Job
 import jp.co.recruit.erikura.data.network.Api
 import jp.co.recruit.erikura.databinding.ActivityPropertyNotesBinding
 import jp.co.recruit.erikura.databinding.FragmentPropertyNotesItemBinding
@@ -40,7 +43,7 @@ class PropertyNotesActivity : BaseActivity(), PropertyNotesEventHandlers {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var placeId: Int = intent.getIntExtra("place_id",0)
+        var placeId: Int = intent.getIntExtra("place_id", 0)
         // 物件の注意事項を取得
         placeId?.let {
             Api(this).placeCautions(it) {
@@ -59,7 +62,8 @@ class PropertyNotesActivity : BaseActivity(), PropertyNotesEventHandlers {
                 }
             }
         }
-        val binding: ActivityPropertyNotesBinding = DataBindingUtil.setContentView(this, R.layout.activity_property_notes)
+        val binding: ActivityPropertyNotesBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_property_notes)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.handlers = this
@@ -77,8 +81,7 @@ class PropertyNotesActivity : BaseActivity(), PropertyNotesEventHandlers {
                     intent.setPackage("com.android.chrome")
                     startActivity(intent)
                 }
-            }
-            catch (e: ActivityNotFoundException) {
+            } catch (e: ActivityNotFoundException) {
                 Intent(Intent.ACTION_VIEW, uri).let { intent ->
                     startActivity(intent)
                 }
@@ -97,17 +100,33 @@ class PropertyNotesActivity : BaseActivity(), PropertyNotesEventHandlers {
         manager.orientation = RecyclerView.VERTICAL
         recyclerView.layoutManager = manager
         //アダプター取得　レイアウトとデータ関連付けさせるため
-        propertyNotesAdapter = PropertyNotesAdapter( this, cautions)
+        propertyNotesAdapter = PropertyNotesAdapter(this, cautions)
         // アダプターをRecyclerViewにセット
         recyclerView.adapter = propertyNotesAdapter
         // アイテム間の幅をセットします
         recyclerView.addItemDecoration(PropertyNotesItemDecorator())
     }
+
+
 }
 
 class PropertyNotesViewModel : ViewModel() {
+    val bitmap: MutableLiveData<Bitmap> = MutableLiveData()
     var address: MutableLiveData<String> = MutableLiveData()
-    val cautions: MutableLiveData<List<Caution>> = MutableLiveData(listOf())
+
+    fun setup(activity: Activity, root: View, file: CautionFile?) {
+        if (file != null){
+            // ダウンロード
+            val imageView: ImageView = root.findViewById(R.id.property_notes_thumbnailImage_image)
+            val thumbnailUrl = if (!file.url.isNullOrBlank()) {file.url} else {null}
+            if (thumbnailUrl.isNullOrBlank()) {
+                imageView.setImageDrawable(ErikuraApplication.instance.applicationContext.resources.getDrawable(R.drawable.ic_noimage, null))
+            }else {
+                val assetsManager = ErikuraApplication.assetsManager
+                assetsManager.fetchImage(activity, thumbnailUrl, imageView)
+            }
+        }
+    }
 }
 
 interface PropertyNotesEventHandlers {
@@ -115,9 +134,10 @@ interface PropertyNotesEventHandlers {
 }
 
 
-class PropertyNotesViewHolder(val binding: FragmentPropertyNotesItemBinding): RecyclerView.ViewHolder(binding.root)
+class PropertyNotesViewHolder(val binding: FragmentPropertyNotesItemBinding) :
+    RecyclerView.ViewHolder(binding.root)
 
-class PropertyNotesItemDecorator: RecyclerView.ItemDecoration() {
+class PropertyNotesItemDecorator : RecyclerView.ItemDecoration() {
     override fun getItemOffsets(
         outRect: Rect,
         view: View,
@@ -132,7 +152,7 @@ class PropertyNotesItemDecorator: RecyclerView.ItemDecoration() {
 class PropertyNotesAdapter(
     val activity: FragmentActivity,
     var cautions: List<Caution>
-    ): RecyclerView.Adapter<PropertyNotesViewHolder>() {
+) : RecyclerView.Adapter<PropertyNotesViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PropertyNotesViewHolder {
         //表示するレイアウトを設定
         val binding = DataBindingUtil.inflate<FragmentPropertyNotesItemBinding>(
@@ -158,33 +178,51 @@ class PropertyNotesAdapter(
         questionTextView.setText("Q.".plus(caution.question))
         answerTextView.setText("A.".plus(caution.answer))
         var files: List<CautionFile> = caution.files
-        // FIXME pdf 画像用のrecyclerViewを呼び出す
 
-//        val propertyNotesItemFileView: ListView = holder.binding.root.findViewById(R.id.property_notes_file_list)
-//        val propertyNotesItemFileAdapter = PropertyNotesItemFileAdapter(activity, LayoutInflater.from(applicationContext),
-//            files as ArrayList<CautionFile>
-//        )
-//        propertyNotesItemFileView.adapter = propertyNotesItemFileAdapter
-//        propertyNotesItemFileAdapter.notifyDataSetChanged()
+        // FIXME pdf 画像用のrecyclerViewを呼び出す
+        if (files.isNotEmpty()) {
+            val propertyNotesItemFileView: ListView =
+                holder.binding.root.findViewById(R.id.property_notes_file_list)
+            val propertyNotesItemFileAdapter = PropertyNotesItemFileAdapter(
+                activity, LayoutInflater.from(applicationContext),
+                files as ArrayList<CautionFile>
+            )
+            propertyNotesItemFileView.adapter = propertyNotesItemFileAdapter
+            propertyNotesItemFileAdapter.notifyDataSetChanged()
+            propertyNotesItemFileView.setOnItemClickListener {parent, view, position, id ->
+                // listViewのクリックされた行のテキストを取得
+                val itemUrl : String = files[position].url
+                // 画像かpdfで分岐
+                //　下記のコメントは削除
+                //またアプリ側の画像拡大表示の実装ですが、他のモーダルと同様に周りをタップすることで閉じるという動作でよろしいでしょうか。
+                //こちらですが、PDFがあることも考えると、
+                // モーダルではなくマニュアルのように下から出てくるダイアログがいいと思います。閉じる時はバツボタン。
+                if (itemUrl.endsWith(".pdf")) {
+                    //マニュアル表示を元にpdfを表示
+//                    pdfの場合リンクを表示しクリックでマニュアルボタンと同じように表示させる
+                }
+                else {
+                    //画像の場合
+                    val item = (view.findViewById<TextView>(android.R.id.text1)).text
+                    // トーストで表示する
+                    Toast.makeText(applicationContext, item, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
 
 class PropertyNotesItemFileAdapter(
     val activity: FragmentActivity,
     var inflater: LayoutInflater,
-    val files: ArrayList<CautionFile>) : BaseAdapter() {
+    val files: ArrayList<CautionFile>
+) : BaseAdapter() {
     internal data class ViewHolder(val image: ImageView)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         var view = convertView
         val viewHolder: ViewHolder
-        if (convertView == null) {
-            view = inflater.inflate(R.layout.fragment_property_notes_item_file, parent)
-            viewHolder = ViewHolder(view.findViewById(R.id.file))
-            view.tag = viewHolder
-        } else {
-            viewHolder = view!!.tag as ViewHolder
-        }
+
         if (files.get(position).url.isNullOrBlank()) {
             //何もセットしない
 //            viewHolder.image.setImageDrawable(
@@ -194,11 +232,33 @@ class PropertyNotesItemFileAdapter(
 //                )
 //            )
         } else {
-            val assetsManager = ErikuraApplication.assetsManager
-            assetsManager.fetchImage(activity, files.get(position).url, viewHolder.image)
+            // 画像かpdfで分岐
+            if (files.get(position).url.endsWith(".pdf")) {
+                if (convertView == null) {
+                    view =
+                        inflater.inflate(R.layout.fragment_property_notes_item_file, parent, false)
+                }
+                //pdfはリンクを表示し押下で、別画面に表示される
+                var textView: TextView = view!!.findViewById(R.id.property_notes_pdf_button)
+                textView.setText(files.get(position).url)
+            } else {
+                if (convertView == null) {
+                    view =
+                        inflater.inflate(R.layout.fragment_property_notes_item_file, parent, false)
+                }
+                Log.v("DEBUG", "files.get(position)があるか${files.get(position)}")
+                Log.v("DEBUG", "files.get(position).urlがあるか${files.get(position).url}")
+
+
+               var thumbnailImageView: ImageView  = view!!.findViewById(R.id.property_notes_thumbnailImage_image)
+                //viewのidとurlとactivityを元にサムネイル画像をセットする
+                val assetsManager = ErikuraApplication.assetsManager
+                assetsManager.fetchImage(activity, files.get(position).url, thumbnailImageView)
+            }
         }
         return view!!
     }
+    //画像の場合サムネで表示しクリックで拡大
 
     override fun getCount(): Int {
         return files.count()
