86c209243959dc2d3c21d191b4c44e0bdb13e41b
RyoyaTanaka <tanaka@cuebs.co.jp>
2020-07-27 19:28:17 +0900

    お手本報告が0件の場合ボタンをとるつめするように実装中(refs=#feature/ERIKURA-1478)

--- app/src/main/java/jp/co/recruit/erikura/presenters/fragments/FinishedJobDetailsFragment.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/fragments/FinishedJobDetailsFragment.kt
@@ -183,6 +183,8 @@ class FinishedJobDetailsFragmentViewModel: BaseJobDetailViewModel() {
     val bitmapDrawable: MutableLiveData<BitmapDrawable> = MutableLiveData()
     val favorited: MutableLiveData<Boolean> = MutableLiveData(false)
     val reportButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.VISIBLE)
+    val reportExamplesButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.VISIBLE)
+
 
     fun setup(activity: Activity, job: Job?, user: User?) {
         this.job.value = job
@@ -218,6 +220,15 @@ class FinishedJobDetailsFragmentViewModel: BaseJobDetailViewModel() {
             if (Api.isLogin) {
                 Api(activity).placeFavoriteShow(job.place?.id ?: 0) {
                     favorited.value = it
+                }
+            }
+
+            //お手本報告件数が0件の場合非表示
+            job.jobKind?.id?.let { jobKindId ->
+                Api(activity).goodExamples(job.placeId, jobKindId, false) {
+                    if (it.count() == 0) {
+                        reportExamplesButtonVisibility.value = View.GONE
+                    }
                 }
             }
         }
