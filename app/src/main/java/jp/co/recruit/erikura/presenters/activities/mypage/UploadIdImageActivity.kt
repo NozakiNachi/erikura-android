998400e5b520b96775186f78f8a2e10810be8599
RyoyaTanaka <tanaka@cuebs.co.jp>
2020-08-21 18:41:53 +0900

    身元確認/身分確認画面の画像入力フィールドの切り分け処理を実装中(refs=#feature/ERIKURA-1570)

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/mypage/UploadIdImageActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/mypage/UploadIdImageActivity.kt
@@ -7,6 +7,8 @@ import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -16,9 +18,17 @@ import jp.co.recruit.erikura.business.models.ComparingData
 import jp.co.recruit.erikura.business.models.IdDocument
 import jp.co.recruit.erikura.business.models.Job
 import jp.co.recruit.erikura.business.models.MediaItem
+import jp.co.recruit.erikura.databinding.ActivityUploadIdImageBinding
 import jp.co.recruit.erikura.presenters.activities.BaseActivity
+import jp.co.recruit.erikura.presenters.activities.job.JobDetailsActivity
+import jp.co.recruit.erikura.presenters.activities.job.MapViewActivity
+import jp.co.recruit.erikura.presenters.activities.tutorial.PermitLocationActivity
 
 class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
+    private val driverLicenceElementNum = 1
+    private val passportElementNum = 4
+    private val myNumberElementNum = 5
+
     var idDocument = IdDocument()
     var comparingData = ComparingData()
     var userId: Int? = null
@@ -30,8 +40,8 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
     }
 
     // 身分証の種別リスト
-    val typeOfIdList =
-        ErikuraApplication.instance.resources.obtainTypedArray(R.array.type_of_id_list)
+    val identityTypeOfList =
+        ErikuraApplication.instance.resources.obtainTypedArray(R.array.identity_type_of_list)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +51,17 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
         if (fromWhere == ErikuraApplication.FROM_ENTRY) {
             job = intent.getParcelableExtra("job")
         }
+
+        val binding: ActivityUploadIdImageBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_upload_id_image)
+        binding.lifecycleOwner = this
+        binding.handlers = this
+        binding.viewModel = viewModel
+
+        // 種別の要素番号
+        viewModel.driverLicenceElementNum.value = driverLicenceElementNum
+        viewModel.passportElementNum.value = passportElementNum
+        viewModel.myNumberElementNum.value = myNumberElementNum
     }
 
     override fun onClickAddPhotoButton(view: View) {
@@ -63,6 +84,7 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        // FIXME 下記をメソッド化
         if (resultCode == Activity.RESULT_OK) {
             val uri = data?.data
             uri?.let {
@@ -80,6 +102,8 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
                     "_id=?", arrayOf(id.split(":")[1]), null
                 )
                 cursor?.moveToFirst()
+                // 上記までメソッド化
+                // FIXME 下記は各ボタンのイベントごとに分けて画像のラベルを切り分ける
                 cursor?.let {
                     // FIXME 対象となるviewModelのvisibility、画像データの反映を切り分ける必要がある
                     // val item = MediaItem.from(cursor)
@@ -110,25 +134,89 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
         finish()
     }
 
+    override fun onClickSkip(view: View) {
+        //遷移元によって遷移先を切り分ける
+        when(fromWhere) {
+            ErikuraApplication.FROM_REGISTER -> {
+                // 地図画面へ
+                if (ErikuraApplication.instance.isOnboardingDisplayed()) {
+                    // 地図画面へ遷移
+                    val intent = Intent(this, MapViewActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                    finish()
+                }
+                else {
+                    // 位置情報の許諾、オンボーディングを表示します
+                    Intent(this, PermitLocationActivity::class.java).let { intent ->
+                        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+            ErikuraApplication.FROM_CHANGE_USER -> {
+                // 元の画面へ
+                finish()
+            }
+            ErikuraApplication.FROM_CHANGE_USER_FOR_CHANGE_INFO -> {
+                // 元の画面へ
+                finish()
+            }
+            ErikuraApplication.FROM_ENTRY -> {
+                // 仕事詳細へ遷移し応募確認ダイアログへ
+                val intent= Intent(this, JobDetailsActivity::class.java)
+                intent.putExtra("fromIdentify", true)
+                intent.putExtra("job", job)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+    }
+
     override fun onClickUploadIdImage(view: View) {
         // FIXME 選択した内容を代入
-        idDocument.type = typeOfIdList.getString(viewModel.typeOfId.value ?: 0)
+        // 画像はリサイズしてBase64にエンコードする
+        idDocument.type = identityTypeOfList.getString(viewModel.typeOfId.value ?: 0)
     }
 }
 
 class UploadIdImageViewModel : ViewModel() {
+    val driverLicenceElementNum = MutableLiveData<Int>()
+    val passportElementNum = MutableLiveData<Int>()
+    val myNumberElementNum = MutableLiveData<Int>()
     // 身分証の種別
     var type: MutableLiveData<String> = MutableLiveData<String>()
     var typeOfId: MutableLiveData<Int> = MutableLiveData<Int>()
+    var normalImageSelectionField = MediatorLiveData<Int>().also { result ->
+        result.addSource(typeOfId) {
+            // パスポート、マイナンバー以外の場合表示
+            if (isNotPassportOrMyNumber()) {
+                result.value = View.VISIBLE
+            } else {
+                result.value = View.GONE
+            }
+        }
+    }
     // 身分証種別によって画像の数をバリデーション、画像サイズ
     private fun isValidTypeOfId(): Boolean {
         return !(typeOfId.value == 0 || typeOfId.value == null)
     }
 
+    // パスポート、マイナンバー以外の場合
+    private fun isNotPassportOrMyNumber(): Boolean {
+        var isNotPassportOrMyNumber = false
+        if (!((typeOfId == passportElementNum) || (typeOfId == myNumberElementNum))) {
+            isNotPassportOrMyNumber = true
+        }
+        return isNotPassportOrMyNumber
+    }
 }
 
 interface UploadIdImageEventHandlers{
     fun onClickClose(view: View)
+    fun onClickSkip(view: View)
     //　FIXME 画像追加ボタンを必須と非必須に分ける
     fun onClickAddPhotoButton(view: View)
     fun onClickUploadIdImage(view: View)
