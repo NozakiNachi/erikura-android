0f9c97eb0bb55a11d9d8d9a54cc5c444a07e5e42
RyoyaTanaka <tanaka@cuebs.co.jp>
2020-08-24 13:58:35 +0900

    身元確認/身分確認画面の画像入力フィールドの切り分け処理を実装中(refs=#feature/ERIKURA-1570)

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/mypage/UploadIdImageActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/mypage/UploadIdImageActivity.kt
@@ -24,10 +24,18 @@ import jp.co.recruit.erikura.presenters.activities.job.JobDetailsActivity
 import jp.co.recruit.erikura.presenters.activities.job.MapViewActivity
 import jp.co.recruit.erikura.presenters.activities.tutorial.PermitLocationActivity
 
-class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
+class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers {
+    // 身分証種別の要素番号
     private val driverLicenceElementNum = 1
     private val passportElementNum = 4
     private val myNumberElementNum = 5
+
+    // 各画像のフィールド識別コード
+    private val frontRequestCode = 1
+    private val backRequestCode = 2
+    private val passportFrontRequestCode = 3
+    private val passportBackRequestCode = 4
+    private val myNumberRequestCode = 5
 
     var idDocument = IdDocument()
     var comparingData = ComparingData()
@@ -47,7 +55,8 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
         super.onCreate(savedInstanceState)
         comparingData = intent.getParcelableExtra("comparingData")
         userId = intent.getIntExtra("userId", 0)
-        fromWhere = intent.getIntExtra(ErikuraApplication.FROM_WHERE, ErikuraApplication.FROM_NOT_FOUND)
+        fromWhere =
+            intent.getIntExtra(ErikuraApplication.FROM_WHERE, ErikuraApplication.FROM_NOT_FOUND)
         if (fromWhere == ErikuraApplication.FROM_ENTRY) {
             job = intent.getParcelableExtra("job")
         }
@@ -58,33 +67,52 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
         binding.handlers = this
         binding.viewModel = viewModel
 
-        // 種別の要素番号
+        // 身分証種別の要素番号
         viewModel.driverLicenceElementNum.value = driverLicenceElementNum
         viewModel.passportElementNum.value = passportElementNum
         viewModel.myNumberElementNum.value = myNumberElementNum
     }
 
-    override fun onClickAddPhotoButton(view: View) {
-        if(ErikuraApplication.instance.hasStoragePermission(this)) {
-            moveToGallery()
-        }
-        else {
+    // 画像選択イベント
+    override fun onClickAddFrontPhotoButton(view: View) {
+        separateTransitionsForEachButtonType(frontRequestCode)
+    }
+
+    override fun onClickAddBackPhotoButton(view: View) {
+        separateTransitionsForEachButtonType(backRequestCode)
+    }
+
+    override fun onCLickAddPassportFrontPhotoButton(view: View) {
+        separateTransitionsForEachButtonType(passportFrontRequestCode)
+    }
+
+    override fun onCLickAddPassportBackPhotoButton(view: View) {
+        separateTransitionsForEachButtonType(passportBackRequestCode)
+    }
+
+    override fun onCLickAddMyNumberPhotoButton(view: View) {
+        separateTransitionsForEachButtonType(myNumberRequestCode)
+    }
+
+    private fun separateTransitionsForEachButtonType(requestCode: Int) {
+        if (ErikuraApplication.instance.hasStoragePermission(this)) {
+            moveToGallery(requestCode)
+        } else {
             ErikuraApplication.instance.requestStoragePermission(this)
         }
     }
 
-    private fun moveToGallery() {
+    private fun moveToGallery(requestCode: Int) {
         val intent = Intent()
         intent.action = Intent.ACTION_OPEN_DOCUMENT
         intent.addCategory(Intent.CATEGORY_OPENABLE)
         intent.type = "image/*"
-        startActivityForResult(intent, 1000 )
+        startActivityForResult(intent, requestCode)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // FIXME 下記をメソッド化
         if (resultCode == Activity.RESULT_OK) {
             val uri = data?.data
             uri?.let {
@@ -102,18 +130,33 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
                     "_id=?", arrayOf(id.split(":")[1]), null
                 )
                 cursor?.moveToFirst()
-                // 上記までメソッド化
                 // FIXME 下記は各ボタンのイベントごとに分けて画像のラベルを切り分ける
                 cursor?.let {
-                    // FIXME 対象となるviewModelのvisibility、画像データの反映を切り分ける必要がある
                     // val item = MediaItem.from(cursor)
                     // MEMO: cursorを渡すとIDの値が0になるので手動で値を入れています
                     val item = MediaItem.from(cursor)
-                    viewModel.addPhotoButtonVisibility.value = View.GONE
-                    viewModel.removePhotoButtonVisibility.value = View.VISIBLE
-                    val imageView: ImageView = findViewById(R.id.report_other_image)
-                    item.loadImage(this, imageView)
-                    viewModel.otherPhoto = item
+                    // requestCodeによって画像フィールドを切り分ける
+                    when (requestCode) {
+                        frontRequestCode -> {
+                            viewModel.addFrontPhotoButtonVisibility.value = View.GONE
+                            viewModel.removeFrontPhotoButtonVisibility.value = View.VISIBLE
+                            val imageView: ImageView = findViewById(R.id.image_front)
+                            item.loadImage(this, imageView)
+                            viewModel.otherPhotoFront = item
+                        }
+                        backRequestCode -> {
+
+                        }
+                        passportFrontRequestCode -> {
+
+                        }
+                        passportBackRequestCode -> {
+
+                        }
+                        myNumberRequestCode -> {
+
+                        }
+                    }
                 }
 
                 cursor?.close()
@@ -136,7 +179,7 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
 
     override fun onClickSkip(view: View) {
         //遷移元によって遷移先を切り分ける
-        when(fromWhere) {
+        when (fromWhere) {
             ErikuraApplication.FROM_REGISTER -> {
                 // 地図画面へ
                 if (ErikuraApplication.instance.isOnboardingDisplayed()) {
@@ -145,11 +188,11 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
                     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                     startActivity(intent)
                     finish()
-                }
-                else {
+                } else {
                     // 位置情報の許諾、オンボーディングを表示します
                     Intent(this, PermitLocationActivity::class.java).let { intent ->
-                        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                         startActivity(intent)
                         finish()
                     }
@@ -165,7 +208,7 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
             }
             ErikuraApplication.FROM_ENTRY -> {
                 // 仕事詳細へ遷移し応募確認ダイアログへ
-                val intent= Intent(this, JobDetailsActivity::class.java)
+                val intent = Intent(this, JobDetailsActivity::class.java)
                 intent.putExtra("fromIdentify", true)
                 intent.putExtra("job", job)
                 startActivity(intent)
@@ -175,9 +218,51 @@ class UploadIdImageActivity : BaseActivity(), UploadIdImageEventHandlers{
 
     }
 
+    // 画像削除イベント
+    override fun onClickRemoveFrontPhoto(view: View) {
+        viewModel.otherPhotoFront = MediaItem()
+        val imageView: ImageView = findViewById(R.id.image_front)
+        imageView.setImageDrawable(null)
+        viewModel.addFrontPhotoButtonVisibility.value = View.VISIBLE
+        viewModel.removeFrontPhotoButtonVisibility.value = View.GONE
+    }
+
+    override fun onClickRemoveBackPhoto(view: View) {
+        viewModel.otherPhotoBack = MediaItem()
+        val imageView: ImageView = findViewById(R.id.image_back)
+        imageView.setImageDrawable(null)
+        viewModel.addBackPhotoButtonVisibility.value = View.VISIBLE
+        viewModel.removeBackPhotoButtonVisibility.value = View.GONE
+    }
+
+    override fun onCLickRemovePassportFrontPhoto(view: View) {
+        viewModel.otherPhotoPassportFront = MediaItem()
+        val imageView: ImageView = findViewById(R.id.passport_front_image)
+        imageView.setImageDrawable(null)
+        viewModel.addPassportFrontPhotoButtonVisibility.value = View.VISIBLE
+        viewModel.removePassportFrontPhotoButtonVisibility.value = View.GONE
+    }
+
+    override fun onClickRemovePassportBackPhoto(view: View) {
+        viewModel.otherPhotoPassportBack = MediaItem()
+        val imageView: ImageView = findViewById(R.id.passport_back_image)
+        imageView.setImageDrawable(null)
+        viewModel.addPassportBackPhotoButtonVisibility.value = View.VISIBLE
+        viewModel.removePassportBackPhotoButtonVisibility.value = View.GONE
+    }
+
+    override fun onClickRemoveMyNumberPhoto(view: View) {
+        viewModel.otherPhotoMyNumber = MediaItem()
+        val imageView: ImageView = findViewById(R.id.my_number_image)
+        imageView.setImageDrawable(null)
+        viewModel.addMyNumberPhotoButtonVisibility.value = View.VISIBLE
+        viewModel.removeMyNumberPhotoButtonVisibility.value = View.GONE
+
+    }
+
     override fun onClickUploadIdImage(view: View) {
         // FIXME 選択した内容を代入
-        // 画像はリサイズしてBase64にエンコードする
+        // FIXME 画像はリサイズしてBase64にエンコードする
         idDocument.type = identityTypeOfList.getString(viewModel.typeOfId.value ?: 0)
     }
 }
@@ -186,10 +271,38 @@ class UploadIdImageViewModel : ViewModel() {
     val driverLicenceElementNum = MutableLiveData<Int>()
     val passportElementNum = MutableLiveData<Int>()
     val myNumberElementNum = MutableLiveData<Int>()
+
     // 身分証の種別
     var type: MutableLiveData<String> = MutableLiveData<String>()
     var typeOfId: MutableLiveData<Int> = MutableLiveData<Int>()
-    var normalImageSelectionField = MediatorLiveData<Int>().also { result ->
+
+    // 表面
+    var otherPhotoFront: MediaItem = MediaItem()
+    val addFrontPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.VISIBLE)
+    val removeFrontPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.GONE)
+
+    // 裏面
+    var otherPhotoBack: MediaItem = MediaItem()
+    val addBackPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.VISIBLE)
+    val removeBackPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.GONE)
+
+    // パスポート表面
+    var otherPhotoPassportFront: MediaItem = MediaItem()
+    val addPassportFrontPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.VISIBLE)
+    val removePassportFrontPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.GONE)
+
+    // パスポート裏面
+    var otherPhotoPassportBack: MediaItem = MediaItem()
+    val addPassportBackPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.VISIBLE)
+    val removePassportBackPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.GONE)
+
+    // マイナンバー
+    var otherPhotoMyNumber: MediaItem = MediaItem()
+    val addMyNumberPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.VISIBLE)
+    val removeMyNumberPhotoButtonVisibility: MutableLiveData<Int> = MutableLiveData(View.GONE)
+
+    // 各画像フィールド表示のvisibility
+    var normalImageSelectionVisibility = MediatorLiveData<Int>().also { result ->
         result.addSource(typeOfId) {
             // パスポート、マイナンバー以外の場合表示
             if (isNotPassportOrMyNumber()) {
@@ -199,9 +312,56 @@ class UploadIdImageViewModel : ViewModel() {
             }
         }
     }
+    var passportImageSelectionVisibility = MediatorLiveData<Int>().also { result ->
+        result.addSource(typeOfId) { id ->
+            // パスポートの場合表示
+            if (id == passportElementNum.value) {
+                result.value = View.VISIBLE
+            } else {
+                result.value = View.GONE
+            }
+        }
+    }
+    var myNumberImageSelectionVisibility = MediatorLiveData<Int>().also { result ->
+        result.addSource(typeOfId) { id ->
+            // パスポートの場合表示
+            if (id == myNumberElementNum.value) {
+                result.value = View.VISIBLE
+            } else {
+                result.value = View.GONE
+            }
+        }
+    }
+
+    //　送信ボタンの活性、非活性
+    val isUploadIdImageButtonEnabled = MediatorLiveData<Boolean>().also { result ->
+        // FIXME バリデーションチェックを行う
+    }
+
+
     // 身分証種別によって画像の数をバリデーション、画像サイズ
     private fun isValidTypeOfId(): Boolean {
         return !(typeOfId.value == 0 || typeOfId.value == null)
+    }
+
+    private fun isValidPhoto(): Boolean {
+        // 身分証種別によって切り分けする
+        when (typeOfId) {
+            passportElementNum -> {
+                return ((otherPhotoPassportFront.contentUri != null) && (otherPhotoPassportBack.contentUri != null))
+            }
+            myNumberElementNum -> {
+                return otherPhotoMyNumber.contentUri != null
+            }
+            else -> {
+                if (removeBackPhotoButtonVisibility.value == View.VISIBLE) {
+                    //　裏面も選択されている場合
+                    return ((otherPhotoBack.contentUri != null) && (otherPhotoFront.contentUri != null))
+                } else {
+                    return otherPhotoFront.contentUri != null
+                }
+            }
+        }
     }
 
     // パスポート、マイナンバー以外の場合
@@ -214,10 +374,24 @@ class UploadIdImageViewModel : ViewModel() {
     }
 }
 
-interface UploadIdImageEventHandlers{
+interface UploadIdImageEventHandlers {
     fun onClickClose(view: View)
     fun onClickSkip(view: View)
-    //　FIXME 画像追加ボタンを必須と非必須に分ける
-    fun onClickAddPhotoButton(view: View)
+
+    //　画像選択イベント
+    fun onClickAddFrontPhotoButton(view: View)
+    fun onClickAddBackPhotoButton(view: View)
+    fun onCLickAddPassportFrontPhotoButton(view: View)
+    fun onCLickAddPassportBackPhotoButton(view: View)
+    fun onCLickAddMyNumberPhotoButton(view: View)
+
+    // 画像削除イベント
+    fun onClickRemoveFrontPhoto(view: View)
+    fun onClickRemoveBackPhoto(view: View)
+    fun onCLickRemovePassportFrontPhoto(view: View)
+    fun onClickRemovePassportBackPhoto(view: View)
+    fun onClickRemoveMyNumberPhoto(view: View)
+
+
     fun onClickUploadIdImage(view: View)
 }
