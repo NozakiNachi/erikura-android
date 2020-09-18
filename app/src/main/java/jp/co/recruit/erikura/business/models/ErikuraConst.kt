1ba0d5756a261d65a0b61e74aec7ab4be6edc419
Takeshi KATO <kato@cuebs.co.jp>
2020-09-18 13:56:46 +0900

    画像選択時の上限枚数を 30枚に制限するように修正

--- app/src/main/java/jp/co/recruit/erikura/business/models/ErikuraConst.kt
+++ app/src/main/java/jp/co/recruit/erikura/business/models/ErikuraConst.kt
@@ -3,4 +3,5 @@ package jp.co.recruit.erikura.business.models
 object ErikuraConst {
     const val maxCommentLength = 2000
     const val maxOutputSummaryCommentLength = 256
+    const val maxOutputSummaries = 30
 }
