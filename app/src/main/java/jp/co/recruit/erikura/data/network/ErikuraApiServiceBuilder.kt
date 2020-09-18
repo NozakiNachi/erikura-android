32e1efd27fcae4b8c80383525549e57ba036f570
Takeshi KATO <kato@cuebs.co.jp>
2020-09-18 13:03:29 +0900

    addSource でのコールバックで null が渡される可能性への対策

--- app/src/main/java/jp/co/recruit/erikura/data/network/ErikuraApiServiceBuilder.kt
+++ app/src/main/java/jp/co/recruit/erikura/data/network/ErikuraApiServiceBuilder.kt
@@ -6,7 +6,6 @@ import jp.co.recruit.erikura.BuildConfig
 import jp.co.recruit.erikura.ErikuraApplication
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
