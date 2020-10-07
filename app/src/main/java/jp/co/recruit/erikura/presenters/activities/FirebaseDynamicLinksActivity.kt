e682e7ab4840c8c99d8f9a23c7c1d76543bde373
TakayamaTokiko <takayama@cuebs.co.jp>
2020-10-07 13:28:31 +0900

    FirebaseDynamicLinksで案件詳細へ遷移と会員本登録への遷移ができるよう修正(ERIKURA-1686)

--- app/src/main/java/jp/co/recruit/erikura/presenters/activities/FirebaseDynamicLinksActivity.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/activities/FirebaseDynamicLinksActivity.kt
@@ -8,6 +8,7 @@ import com.google.firebase.dynamiclinks.ktx.dynamicLinks
 import com.google.firebase.ktx.Firebase
 import jp.co.recruit.erikura.business.models.Job
 import jp.co.recruit.erikura.presenters.activities.job.JobDetailsActivity
+import jp.co.recruit.erikura.presenters.activities.registration.RegisterPasswordActivity
 
 class FirebaseDynamicLinksActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +21,23 @@ class FirebaseDynamicLinksActivity : BaseActivity() {
                     deepLink = pendingDynamicLinkData.link
                 }
 
+                var intent = Intent(this, StartActivity::class.java)
                 if (deepLink != null) {
-                    val job = Job(id= deepLink.lastPathSegment?.toInt() ?: 0)
-                    job.uninitialized = true
-                    val intent = Intent(this, JobDetailsActivity::class.java)
-                    intent.putExtra("job", job)
-                    intent.putExtra("jobRestored", false)
-                    startActivity(intent)
-                    finish()
+                    if (deepLink.toString().contains("jobs/")) {
+                        // 案件詳細画面へ遷移します
+                        val job = Job(id= deepLink.lastPathSegment?.toInt() ?: 0)
+                        job.uninitialized = true
+                        intent = Intent(this, JobDetailsActivity::class.java)
+                        intent.putExtra("job", job)
+                        intent.putExtra("jobRestored", false)
+                    }else {
+                        // 本登録画面へ遷移します
+                        intent = Intent(this, RegisterPasswordActivity::class.java)
+                        intent.data = deepLink
+                    }
                 }
+                startActivity(intent)
+                finish()
             }
             .addOnFailureListener(this) { e ->
                 Log.w("JobDetailsActivity onCreate", "getDynamicLink:onFailure", e)
