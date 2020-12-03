730b932fb8ca7acf4f255ec4ae53b89d3231100a
RyoyaTanaka <tanaka@cuebs.co.jp>
2020-12-03 16:30:55 +0900

    128文字以内のバリデーションを追加(refs=#feature/ERIKURA-1998)

--- app/src/main/java/jp/co/recruit/erikura/business/models/User.kt
+++ app/src/main/java/jp/co/recruit/erikura/business/models/User.kt
@@ -33,6 +33,7 @@ data class User(
 ) : Parcelable {
     companion object {
         val pattern = Pattern.compile("^([a-zA-Z0-9]{6,})\$")
+        val overPattern = Pattern.compile("^([a-zA-Z0-9])\$")
         val alPattern = Pattern.compile("^(.*[A-z]+.*)")
         val numPattern = Pattern.compile("^(.*[0-9]+.*)")
         val hasAlphabet: (str: String) -> Boolean = { str -> alPattern.matcher(str).find() }
@@ -47,7 +48,13 @@ data class User(
                     valid = false
                     passwordErrorMessage =
                         ErikuraApplication.instance.getString(R.string.password_count_error)
-                } else if (valid && !(hasAlphabet(pwd) && hasNumeric(pwd))) {
+                }
+                else if(valid && (!(overPattern.matcher(pwd).find()) && !(pwd.length <= 128))) {
+                    valid = false
+                    passwordErrorMessage =
+                        ErikuraApplication.instance.getString(R.string.password_count_over_error)
+                }
+                else if (valid && !(hasAlphabet(pwd) && hasNumeric(pwd))) {
                     valid = false
                     passwordErrorMessage =
                         ErikuraApplication.instance.getString(R.string.password_pattern_error)
