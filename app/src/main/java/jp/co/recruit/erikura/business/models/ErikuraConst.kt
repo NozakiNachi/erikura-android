1b96b3a9db9d3b6eb4b3c7ea1cdd8dba20fdda53
Takeshi KATO <kato@cuebs.co.jp>
2021-02-17 11:41:19 +0900

    実施箇所の上限件数を変更

--- app/src/main/java/jp/co/recruit/erikura/business/models/ErikuraConst.kt
+++ app/src/main/java/jp/co/recruit/erikura/business/models/ErikuraConst.kt
@@ -3,6 +3,6 @@ package jp.co.recruit.erikura.business.models
 object ErikuraConst {
     const val maxCommentLength = 2000
     const val maxOutputSummaryCommentLength = 256
-    const val maxOutputSummaries = 30
+    const val maxOutputSummaries = 100
     val emailPattern = """\A[\w._%+-|]+@[\w0-9.-]+\.[A-Za-z]{2,}\z""".toRegex()
 }
