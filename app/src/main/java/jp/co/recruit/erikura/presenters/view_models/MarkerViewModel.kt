7e5a256527f90388f5a5087b1a1db076e0167bd3
RyoyaTanaka <tanaka@cuebs.co.jp>
2021-04-14 12:08:11 +0900

    先行応募中の案件はマーカーを活性にする(refs=#feature/ERIKURA-2472)

--- app/src/main/java/jp/co/recruit/erikura/presenters/view_models/MarkerViewModel.kt
+++ app/src/main/java/jp/co/recruit/erikura/presenters/view_models/MarkerViewModel.kt
@@ -20,7 +20,7 @@ open class MarkerViewModel(val job: Job): ViewModel() {
     val icon: MutableLiveData<Bitmap> = MutableLiveData()
 
     val fee: String get() = String.format("%,d円", job.fee)
-    open val isDisabled: Boolean get() = (job.isEntried || job.isFuture) && !isOwnerMarker
+    open val isDisabled: Boolean get() = (job.isEntried || (job.isFuture && !(job.isPreEntry))) && !isOwnerMarker
     val iconUrl: URL? get () {
         if (isDisabled) {
             return job.jobKind?.inactiveIconUrl
